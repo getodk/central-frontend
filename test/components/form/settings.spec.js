80b0a294851783d36fc1641e5e33b9a83e7453a0
Matthew White <matthew-white@users.noreply.github.com>
2018-05-16 23:16:25 -0400

    Add tests of existing settings page

---
+++ test/components/form/settings.spec.js
@@ -0,0 +1,36 @@
+/*
+Copyright 2017 Super Adventure Developers
+See the NOTICE file at the top-level directory of this distribution and at
+https://github.com/nafundi/super-adventure/blob/master/NOTICE.
+
+This file is part of Super Adventure. It is subject to the license terms in
+the LICENSE file found in the top-level directory of this distribution and at
+https://www.apache.org/licenses/LICENSE-2.0. No part of Super Adventure,
+including this file, may be copied, modified, propagated, or distributed
+except according to the terms contained in the LICENSE file.
+*/
+import testData from '../../data';
+import { mockRoute } from '../../http';
+import { mockRouteThroughLogin } from '../../session';
+
+const settingsPath = (form) => `/forms/${form.xmlFormId}/settings`;
+
+describe('FormSettings', () => {
+  describe('routing', () => {
+    it('redirects an anonymous user to login', () =>
+      mockRoute(settingsPath(testData.simpleForms.createPast(1).last()))
+        .then(app => app.vm.$route.path.should.equal('/login')));
+
+    it('redirects the user back after login', () => {
+      const path = settingsPath(testData.simpleForms.createPast(1).last());
+      return mockRouteThroughLogin(path)
+        .respondWithData(() => testData.simpleForms.last())
+        .afterResponse(app => app.vm.$route.path.should.equal(path));
+    });
+  });
+
+  it('shows a success message after login', () =>
+    mockRouteThroughLogin(settingsPath(testData.simpleForms.createPast(1).last()))
+      .respondWithData(() => testData.simpleForms.last())
+      .afterResponse(app => app.should.alert('success')));
+});
