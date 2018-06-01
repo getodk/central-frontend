c1d06486add658ab9ab68be72aa9bf2f900dbee9
Matthew White <matthew-white@users.noreply.github.com>
2018-06-01 02:51:50 -0400

    Add backdrop to reset password modal

--- lib/components/user/reset-password.vue
+++ lib/components/user/reset-password.vue
@@ -10,7 +10,8 @@ including this file, may be copied, modified, propagated, or distributed
 except according to the terms contained in the LICENSE file.
 -->
 <template>
-  <modal :state="state" :hideable="!awaitingResponse" @hide="$emit('hide')">
+  <modal :state="state" :hideable="!awaitingResponse" backdrop
+    @hide="$emit('hide')">
     <template slot="title">Reset Password</template>
     <template slot="body">
       <alert v-bind="alert" @close="alert.state = false"/>
