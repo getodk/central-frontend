eac7d9a965627ff29640b78fef9bfa8b4e0e6428
Matthew White <matthew-white@users.noreply.github.com>
2018-06-05 16:14:24 -0400

    Linting

--- lib/components/form/list.vue
+++ lib/components/form/list.vue
@@ -17,8 +17,8 @@ except according to the terms contained in the LICENSE file.
         You collect data by
         <doc-link to="form-design-intro/">designing a form</doc-link> on your
         computer, and then
-        <doc-link to="collect-forms/#loading-blank-forms">loading that form onto a mobile device</doc-link>
-        to administer.
+        <doc-link to="collect-forms/#loading-blank-forms">loading that form onto
+        a mobile device</doc-link> to administer.
       </template>
     </page-head>
     <page-body>
