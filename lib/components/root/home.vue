53c03aa0f77a8ff730f29fd58d58f3a0fee87b43
Matthew White <matthew-white@users.noreply.github.com>
2018-06-15 04:45:20 -0400

    Add site root page

---
+++ lib/components/root/home.vue
@@ -0,0 +1,103 @@
+<!--
+Copyright 2017 ODK Central Developers
+See the NOTICE file at the top-level directory of this distribution and at
+https://github.com/opendatakit/central-frontend/blob/master/NOTICE.
+
+This file is part of ODK Central. It is subject to the license terms in
+the LICENSE file found in the top-level directory of this distribution and at
+https://www.apache.org/licenses/LICENSE-2.0. No part of ODK Central,
+including this file, may be copied, modified, propagated, or distributed
+except according to the terms contained in the LICENSE file.
+-->
+<template>
+  <div>
+    <div id="root-home-heading">
+      <h1>Welcome to Central.</h1>
+      <p>Let's get some things done.</p>
+    </div>
+    <loading :state="fetchCounter < 3"/>
+    <div v-show="fetchCounter === 3">
+      <div class="panel panel-simple">
+        <div class="panel-heading"><h1 class="panel-title">You Have:</h1></div>
+        <div class="panel-body">
+          <div class="row">
+            <div class="col-xs-6">
+              <root-entity :icons="['user-circle', 'cog']" name="Web User"
+                data-from="/users" route-to="/users"
+                @fetched="incrementCounter">
+                who are allowed to administer projects through this website.
+              </root-entity>
+            </div>
+            <div class="col-xs-6">
+              <root-entity :icons="['user-circle', 'mobile']" name="App User"
+                data-from="/field-keys" route-to="/users/field-keys"
+                @fetched="incrementCounter">
+                who can use a data collection client to download and submit form
+                data to this server.
+              </root-entity>
+            </div>
+          </div>
+          <hr>
+          <root-entity :icons="['file-text']" name="Form"
+            data-from="/forms" route-to="/forms"
+            @fetched="incrementCounter">
+            which can be downloaded and administered as surveys on mobile
+            clients.
+          </root-entity>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-6">
+          <div class="panel panel-simple">
+            <div class="panel-heading">
+              <h1 class="panel-title">Getting Started</h1>
+            </div>
+            <div class="panel-body">
+              <p>
+                If you’re not sure where to begin, we have a getting started
+                guide and user documentation available on the
+                <doc-link>Open Data Kit Docs website</doc-link>.
+              </p>
+              <p>
+                In addition, you can always get help from others on the
+                <a href="https://forum.opendatakit.org/" target="_blank">Open Data Kit community forum</a>,
+                where you can search for previous answers or ask one of your
+                own.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import RootEntity from './entity.vue';
+
+export default {
+  name: 'RootHome',
+  components: { RootEntity },
+  data() {
+    return {
+      fetchCounter: 0
+    };
+  },
+  methods: {
+    incrementCounter() {
+      this.fetchCounter += 1;
+    }
+  }
+};
+</script>
+
+<style lang="sass">
+#root-home-heading {
+  margin-bottom: 20px;
+  margin-top: 30px;
+
+  p {
+    font-size: 20px;
+  }
+}
+</style>
---
+++ test/components/root/home.spec.js
@@ -0,0 +1,82 @@
+import faker from '../../faker';
+import testData from '../../data';
+import { mockLogin, mockRouteThroughLogin } from '../../session';
+import { mockRoute } from '../../http';
+import { trigger } from '../../util';
+
+describe('RootHome', () => {
+  describe('anonymous users', () => {
+    it('routes an anonymous user to login', () =>
+      mockRoute('/').then(app => app.vm.$route.path.should.equal('/login')));
+
+    it('routes the user back after login', () =>
+      mockRouteThroughLogin('/')
+        .respondWithProblem()
+        .respondWithProblem()
+        .respondWithProblem()
+        .afterResponses(app => app.vm.$route.path.should.equal('/')));
+  });
+
+  describe('after login', () => {
+    beforeEach(mockLogin);
+
+    it('routes user to site root after user clicks on navbar header', () =>
+      mockRoute('/forms')
+        .respondWithData(() => testData.forms.sorted())
+        .complete()
+        .request(app => trigger.click(app.first('.navbar-brand')))
+        .respondWithProblem()
+        .respondWithProblem()
+        .respondWithProblem()
+        .afterResponses(app => app.vm.$route.path.should.equal('/')));
+
+    it('shows the correct counts', () => {
+      const users = faker.random.number();
+      const fieldKeys = faker.random.number();
+      const forms = faker.random.number();
+      // Using mockRoute() rather than mockHttp() because the page contains
+      // <router-link> tags.
+      return mockRoute('/')
+        // We don't actually fill the array with data, as the page does not use
+        // the data.
+        .respondWithData(() => new Array(users))
+        .respondWithData(() => new Array(fieldKeys))
+        .respondWithData(() => new Array(forms))
+        .afterResponses(app => {
+          const counts = app
+            .find('.root-entity-count a')
+            .map(a => a.text().trim());
+          counts.length.should.equal(3);
+          counts[0].should.equal(users.toLocaleString());
+          counts[1].should.equal(fieldKeys.toLocaleString());
+          counts[2].should.equal(forms.toLocaleString());
+        });
+    });
+
+    describe('clicking anywhere on the block routes to correct page', () => {
+      const routes = ['/users', '/users/field-keys', '/forms'];
+      for (let i = 0; i < routes.length; i += 1) {
+        it(`renders 4 links in entity ${i}`, () =>
+          mockRoute('/')
+            .respondWithData(() => new Array(1))
+            .respondWithData(() => new Array(1))
+            .respondWithData(() => new Array(1))
+            .afterResponses(app =>
+              app.find('.root-entity')[i].find('a').length.should.equal(4)));
+
+        it(`routes to correct page after user clicks a link in entity ${i}`, () =>
+          mockRoute('/')
+            .respondWithData(() => new Array(1))
+            .respondWithData(() => new Array(1))
+            .respondWithData(() => new Array(1))
+            .complete()
+            .request(app => {
+              const a = app.find('.root-entity')[i].find('a');
+              return trigger.click(faker.random.arrayElement(a));
+            })
+            .respondWithProblem()
+            .afterResponse(app => app.vm.$route.path.should.equal(routes[i])));
+      }
+    });
+  });
+});
