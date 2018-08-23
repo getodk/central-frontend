67b4767fb387f3ea2334b16525d2599daf885646
Matthew White <matthew-white@users.noreply.github.com>
2018-08-23 13:35:27 -0400

    Replace mockHttp().route() with mockRoute()

--- test/components/not-found.spec.js
+++ test/components/not-found.spec.js
@@ -1,15 +1,13 @@
-import { mockHttp } from '../http';
+import { mockRoute } from '../http';
 
 describe('NotFound', () => {
   it('does not send a request, including to restore the session', () =>
-    mockHttp()
-      .route('/not-found'));
+    mockRoute('/not-found'));
 
   // This might not be the ideal behavior, but this test documents the current
   // behavior.
   it('navigating away does not restore the session', () =>
-    mockHttp()
-      .route('/not-found')
+    mockRoute('/not-found')
       .complete()
       // Route without specifying a response.
       .route('/login'));
