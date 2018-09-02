ad45c13c14f14e9548d1e7c7db2a68e351991031
Matthew White <matthew-white@users.noreply.github.com>
2018-09-02 15:09:41 -0400

    Add `selector` parameter for trigger() in testing

--- test/util.js
+++ test/util.js
@@ -19,14 +19,16 @@ export class MockLogger {
 }
 
 // Triggers an event that does not bubble.
-export const trigger = (eventName, wrapper) => {
-  wrapper.trigger(eventName);
-  return Vue.nextTick();
+export const trigger = (eventName, wrapper, selector = undefined) => {
+  const target = selector == null ? wrapper : wrapper.first(selector);
+  target.trigger(eventName);
+  const nextTick = Vue.nextTick();
+  return selector == null ? nextTick : nextTick.then(() => wrapper);
 };
 
 const eventNames = ['change', 'click', 'submit'];
 for (const name of eventNames)
-  trigger[name] = (wrapper) => trigger(name, wrapper);
+  trigger[name] = (wrapper, selector = undefined) => trigger(name, wrapper, selector);
 
 export const fillForm = (wrapper, selectorsAndValues) => {
   let promise = Promise.resolve();
