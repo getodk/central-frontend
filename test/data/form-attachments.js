ee1156bc2d65dab304c0f39f91325c0635a67668
Matthew White <matthew-white@users.noreply.github.com>
2018-09-04 10:32:53 -0400

    Update test data for form attachments

--- test/data/form-attachments.js
+++ test/data/form-attachments.js
@@ -2,31 +2,46 @@ import faker from '../faker';
 import { dataStore } from './data-store';
 import { extendedForms } from './forms';
 
-const FORM_ATTACHMENT_TYPES = [
-  { type: 'image', fileExtension: 'jpg' },
-  { type: 'audio', fileExtension: 'mp3' },
-  { type: 'video', fileExtension: 'mp4' }
-];
+const TYPES = {
+  image: 'jpg',
+  audio: 'mp3',
+  video: 'mp4'
+};
 
 // eslint-disable-next-line import/prefer-default-export
 export const extendedFormAttachments = dataStore({
   factory: ({
     inPast,
-    lastCreatedAt,
-    exists = faker.random.boolean()
+    type = faker.random.arrayElement(Object.keys(TYPES)),
+    // Adding a UUID to help ensure uniqueness.
+    name = `${faker.random.words()} ${faker.random.uuid()}${TYPES[type]}`,
+    ...options
   }) => {
+    if (!inPast && options.exists === true)
+      throw new Error('inPast and exists are inconsistent');
+    if (options.hasUpdatedAt != null) {
+      if (!inPast && options.hasUpdatedAt)
+        throw new Error('inPast and hasUpdatedAt are inconsistent');
+      if (options.exists === true && !options.hasUpdatedAt)
+        throw new Error('exists and hasUpdatedAt are inconsistent');
+      if (options.exists == null) {
+        // eslint-disable-next-line no-param-reassign
+        options.exists = options.hasUpdatedAt ? faker.random.boolean() : false;
+      }
+    } else {
+      if (options.exists == null) {
+        // eslint-disable-next-line no-param-reassign
+        options.exists = inPast && faker.random.boolean();
+      }
+      // eslint-disable-next-line no-param-reassign
+      options.hasUpdatedAt = inPast && (options.exists || faker.random.boolean());
+    }
+    const { exists, hasUpdatedAt } = options;
     const form = extendedForms.randomOrCreatePast();
-    const type = faker.random.arrayElement(FORM_ATTACHMENT_TYPES);
-    const { updatedAt } = faker.date.timestamps(inPast, [
-      lastCreatedAt,
-      form.createdAt
-    ]);
-    return {
-      type,
-      name: faker.system.commonFileName(type.fileExtension),
-      exists,
-      updatedAt
-    };
+    const updatedAt = hasUpdatedAt
+      ? faker.date.pastSince(form.createdAt).toISOString()
+      : null;
+    return { type, name, exists, updatedAt };
   },
   sort: 'name'
 });
