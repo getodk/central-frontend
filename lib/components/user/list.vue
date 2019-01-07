fc551c678913ffad64bee7a803a74ddbbbe22e3b
Matthew White <matthew-white@users.noreply.github.com>
2019-01-07 12:44:24 -0500

    Capitalize "Web User" in UserList

--- lib/components/user/list.vue
+++ lib/components/user/list.vue
@@ -16,7 +16,7 @@ except according to the terms contained in the LICENSE file.
         @refresh="fetchData({ clear: false })"/>
       <button id="user-list-new-button" slot="right" type="button"
         class="btn btn-primary" @click="showModal('newUser')">
-        <span class="icon-plus-circle"></span>Create web user
+        <span class="icon-plus-circle"></span>Create Web User
       </button>
     </float-row>
     <loading v-if="users == null" :state="awaitingResponse"/>
