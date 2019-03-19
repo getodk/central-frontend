75b43c5316c0371d97581f7b7442b7a2e35ac76e
Matthew White <matthew-white@users.noreply.github.com>
2019-03-19 12:39:34 -0400

    Use clearer name for function in assertions.js

--- test/assertions.js
+++ test/assertions.js
@@ -3,7 +3,7 @@ import should from 'should';
 import Alert from '../lib/components/alert.vue';
 
 // Returns the element for an object that may be an avoriaz wrapper.
-const element = (elementOrWrapper) => {
+const unwrapElement = (elementOrWrapper) => {
   if (elementOrWrapper.isVueComponent === true) return elementOrWrapper.vm.$el;
   return elementOrWrapper.element != null
     ? elementOrWrapper.element
@@ -12,19 +12,19 @@ const element = (elementOrWrapper) => {
 
 should.Assertion.add('visible', function visible() {
   this.params = { operator: 'to be visible' };
-  element(this.obj).style.display.should.equal('');
+  unwrapElement(this.obj).style.display.should.equal('');
 });
 
 should.Assertion.add('hidden', function hidden() {
   this.params = { operator: 'to be hidden' };
-  element(this.obj).style.display.should.equal('none');
+  unwrapElement(this.obj).style.display.should.equal('none');
 });
 
 // If a test does not attach the component to the document, then uses this
 // assertion, it may time out rather than fail. (I am not sure why.)
 should.Assertion.add('focused', function focused() {
   this.params = { operator: 'to be focused' };
-  element(this.obj).should.equal(document.activeElement);
+  unwrapElement(this.obj).should.equal(document.activeElement);
 });
 
 should.Assertion.add('alert', function assertAlert(type = undefined, message = undefined) {
