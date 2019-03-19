01cb16cec2abc3fb80b9509591050a85c4586ba9
Matthew White <matthew-white@users.noreply.github.com>
2019-03-19 12:20:33 -0400

    Add sections to test/setup.js

--- test/setup.js
+++ test/setup.js
@@ -16,27 +16,54 @@ import { destroyMarkedComponent, mountAndMark } from './destroy';
 import { setHttp } from './http';
 import './assertions';
 
+
+
+////////////////////////////////////////////////////////////////////////////////
+// STANDARD BUILT-IN OBJECTS
+
 // "iTrim" for "internally trim"
 // eslint-disable-next-line no-extend-native
 String.prototype.iTrim = function iTrim() {
   return this.replace(/\s+/g, ' ');
 };
 
+
+
+////////////////////////////////////////////////////////////////////////////////
+// UTILITIES
+
 Vue.prototype.$logger = new MockLogger();
 
-// Set up the router for testing.
+// We set $http below.
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// ROUTER
+
 // Inject the router into a Vue instance so that tests can assume that the
 // router has been injected into at least one Vue instance.
 setHttp(() => Promise.reject(new Error()));
 mountAndMark(App, { router });
 destroyMarkedComponent();
+
 initNavGuards();
 afterEach(clearNavGuards);
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// $http
 
 setHttp(config => {
   console.log('unhandled request', config); // eslint-disable-line no-console
   return Promise.reject(new Error());
 });
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// DESTROY COMPONENT
 
 afterEach(() => {
   destroyMarkedComponent();
@@ -49,7 +76,12 @@ afterEach(() => {
   }
 });
 
-// Reset the Vuex store.
+
+
+////////////////////////////////////////////////////////////////////////////////
+// VUEX
+
+// Reset the store.
 afterEach(() => {
   store.commit('resetAlert');
   store.commit('resetRequests');
@@ -59,5 +91,16 @@ afterEach(() => {
   // perhaps it would be better to move that code here?)
 });
 
+
+
+////////////////////////////////////////////////////////////////////////////////
+// TEST DATA
+
 afterEach(testData.reset);
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// FAKER
+
 afterEach(clearUniqueFakerResults);
