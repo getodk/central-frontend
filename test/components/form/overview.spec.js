8147b21244c3f50759862634462034c5076da431
Matthew White <matthew-white@users.noreply.github.com>
2019-06-10 10:32:57 -0400

    Move general FormOverview tests out of FormChecklist tests

---
+++ test/components/form/overview.spec.js
@@ -0,0 +1,25 @@
+import testData from '../../data';
+import { mockRoute } from '../../http';
+import { mockRouteThroughLogin } from '../../session';
+
+describe('FormOverview', () => {
+  describe('anonymous users', () => {
+    it('redirects an anonymous user to login', () =>
+      mockRoute('/projects/1/forms/f')
+        .restoreSession(false)
+        .afterResponse(app => {
+          app.vm.$route.path.should.equal('/login');
+        }));
+
+    it('redirects the user back after login', () =>
+      mockRouteThroughLogin('/projects/1/forms/f')
+        .respondWithData(() => testData.extendedProjects.createPast(1).last())
+        .respondWithData(() => testData.extendedForms
+          .createPast(1, { xmlFormId: 'f' })
+          .last())
+        .respondWithData(() => testData.extendedFormAttachments.sorted())
+        .afterResponses(app => {
+          app.vm.$route.path.should.equal('/projects/1/forms/f');
+        }));
+  });
+});
