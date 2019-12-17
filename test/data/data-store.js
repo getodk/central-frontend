ef8b43f992c4e40d7921784e7a06804bb36e09a8
Matthew White <matthew-white@users.noreply.github.com>
2019-12-17 11:35:15 -0500

    Simplify and clarify test data
    Closes #143.

--- test/data/data-store.js
+++ test/data/data-store.js
@@ -1,114 +1,29 @@
 import faker from '../faker';
-import { uniqueSequence } from '../../src/util/util';
 
-class Factory {
-  constructor(store, options) {
-    this._store = store;
-    this._options = { ...options };
-    this.reset();
-  }
-
-  reset() {
-    this._uniqueId = uniqueSequence();
-    this._lastCreatedAt = null;
-  }
-
-  options() { return this._options; }
-
-  /*
-  newObject() returns a new valid object for the store. It can be called in any
-  of the following ways:
-
-    newObject(inPast)
-    newObject(inPast, constraintOrConstraints)
-    newObject(inPast, factoryFunctionOptions)
-
-  with the following parameters:
-
-    - inPast. If true, the value of the new object's createdAt property will be
-      in the past. If false, the value will be set to the current time. `inPast`
-      has no effect if the Factory's options specify that new objects be created
-      without a createdAt property.
-    - constraintOrConstraints. Either a String or an Array of Strings, where
-      each String is the name of one of the constraints specified in the
-      Factory's options. The constraints with the specified names will be
-      applied to the new object.
-    - factoryFunctionOptions. Options to pass to the factory function. Not all
-      factory functions require options.
-
-  Constraints and factory function options fill similar roles: they both specify
-  information about the new object. However, they do so in different ways.
-  Unlike constraints, factory function options are passed to the factory
-  function, which uses the options while creating each new object candidate.
-  After a candidate is returned, it is validated against the constraints and is
-  rejected if it does not satisfy them.
-
-  Constraints are useful for specifying a particular condition, for example,
-  "the form state is open" or "the app user's access was revoked." Factory
-  function options are useful for specifying a particular property, for example,
-  "the form name is 'Household Survey'." Factory function options can be used to
-  pass any information to the factory function, so constraints can usually be
-  implemented using factory function options. Because a constraint failure
-  results in the creation of a new object candidate, factory function options
-  are usually more performant than constraints. We may remove constraints at
-  some point and replace them with factory function options.
-  */
-  newObject(inPast, constraintsOrOptions = undefined) {
-    const { constraints, factoryFunctionOptions } =
-      this._constraintsAndOptions(constraintsOrOptions);
-    const { factory } = this._options;
-    const id = this._uniqueId();
-    for (;;) {
-      const object = factory({
-        ...factoryFunctionOptions,
-        inPast,
-        id,
-        lastCreatedAt: this._lastCreatedAt
-      });
-      if (this._isValid(object, constraints)) {
-        this._lastCreatedAt = object.createdAt;
-        return object;
-      }
-    }
-  }
-
-  _constraintsAndOptions(constraintsOrOptions) {
-    if (constraintsOrOptions == null)
-      return { constraints: [], factoryFunctionOptions: {} };
-    if (typeof constraintsOrOptions === 'object' && !Array.isArray(constraintsOrOptions))
-      return { constraints: [], factoryFunctionOptions: constraintsOrOptions };
-    const constraintNames = Array.isArray(constraintsOrOptions)
-      ? constraintsOrOptions
-      : [constraintsOrOptions];
-    if (constraintNames.length > 0 && this._options.constraints == null)
-      throw new Error('no constraints are defined for the factory');
-    const constraints = constraintNames.map(name => {
-      const constraint = this._options.constraints[name];
-      if (constraint == null) throw new Error('unknown constraint');
-      return constraint;
-    });
-    return { constraints, factoryFunctionOptions: {} };
-  }
-
-  _isValid(object, constraints) {
-    for (const validator of constraints)
-      if (!validator(object, this._store)) return false;
-    return true;
-  }
-}
-
+// An array-like collection of objects. Objects may be created, read, sorted,
+// and deleted. The objects are ordered within the collection in order of
+// creation.
 class Collection {
-  // eslint-disable-next-line no-unused-vars
-  createPast(count, constraintsOrOptions = undefined) { throw new Error('not implemented'); }
-  // eslint-disable-next-line no-unused-vars
-  createNew(constraintsOrOptions = undefined) { throw new Error('not implemented'); }
+  /* eslint-disable no-unused-vars */
+
+  // Generates an object and adds it to the collection. If the object has a
+  // createdAt property, it will be in the past. Returns the collection for
+  // chaining.
+  createPast(count, options = undefined) { throw new Error('not implemented'); }
+  // Generates an object and adds it to the collection. If the object has a
+  // createdAt property, it will be set to the current time. Returns the new
+  // object.
+  createNew(options = undefined) { throw new Error('not implemented'); }
+  // Returns the number of objects in the store.
   get size() { throw new Error('not implemented'); }
-  // eslint-disable-next-line no-unused-vars
+  // Returns the object at the specified index.
   get(index) { throw new Error('not implemented'); }
-  clear() { throw new Error('not implemented'); }
-  // eslint-disable-next-line no-unused-vars
-  splice(start, deleteCount) { throw new Error('not implemented'); }
+  // Returns the objects of the collection as an Array that is sorted in some
+  // way (in order of creation or otherwise).
   sorted() { throw new Error('not implemented'); }
+  splice(start, deleteCount) { throw new Error('not implemented'); }
+
+  /* eslint-enable no-unused-vars */
 
   first() { return this.size !== 0 ? this.get(0) : undefined; }
   last() { return this.size !== 0 ? this.get(this.size - 1) : undefined; }
@@ -120,51 +35,106 @@ class Collection {
 
   firstOrCreatePast() {
     if (this.size === 0) this.createPast(1);
-    return this.first();
+    return this.get(0);
   }
 
   randomOrCreatePast() {
-    if (this.size !== 0) return this.random();
-    this.createPast(1);
-    return this.first();
+    return this.size !== 0
+      ? this.random()
+      : this.createPast(1).get(0);
   }
 }
 
+// Implements the methods of Collection.
 class Store extends Collection {
-  constructor({ sort, ...factoryOptions }) {
+  constructor({
+    // A factory function that generates objects for the store. createPast() and
+    // createNew() pass arguments to the factory; see _create() for details.
+    factory,
+    // A compare function that sorted() will use to sort the objects in the
+    // store
+    sort = undefined
+  }) {
     super();
-    this._factory = new Factory(this, factoryOptions);
+    // When we run the callback later, we do not want it to be bound to the
+    // Store.
+    this._factory = factory.bind(null);
     this._compareFunction = sort;
-    this.clear();
+    this.reset();
   }
 
-  createPast(count, constraintsOrOptions = undefined) {
+  reset() {
+    this._objects = [];
+    this._createdNew = false;
+    this._id = 0;
+    this._lastCreatedAt = null;
+  }
+
+  createPast(count, options = undefined) {
+    if (count === 0) return this;
+    // We could remove this restriction, but it would require reworking
+    // this._lastCreatedAt and maybe other things.
     if (this._createdNew)
       throw new Error('createPast() is not allowed after createNew()');
-    for (let i = 0; i < count; i += 1) {
-      const object = this._factory.newObject(true, constraintsOrOptions);
-      this._objects.push(object);
-    }
+    for (let i = 0; i < count; i += 1)
+      this._create(true, options);
     return this;
   }
 
-  createNew(constraintsOrOptions = undefined) {
-    const object = this._factory.newObject(false, constraintsOrOptions);
-    this._objects.push(object);
+  createNew(options = undefined) {
+    const object = this._create(false, options);
     this._createdNew = true;
+    return object;
+  }
+
+  _create(inPast, options = undefined) {
+    this._id += 1;
+    const object = this._factory({
+      ...options,
+
+      // We always pass the following arguments to the factory, but they might
+      // not be relevant to all factories.
+
+      // inPast is relevant to factories that return objects with a createdAt
+      // property. If inPast is `true`, the createdAt property of the resulting
+      // object should be in the past. If it is `false`, its createdAt property
+      // should be set to the current time.
+      inPast,
+      // id is relevant to factories that return objects with an `id` property.
+      id: this._id,
+      // lastCreatedAt is relevant to factories that return objects with a
+      // createdAt property. The createdAt property of the resulting object
+      // should be greater than or equal to lastCreatedAt.
+      lastCreatedAt: this._lastCreatedAt
+    });
+    this._objects.push(object);
+    this._lastCreatedAt = object.createdAt;
     return object;
   }
 
   get size() { return this._objects.length; }
   get(index) { return this._objects[index]; }
 
-  // update() updates an existing object, setting the properties specified by
+  sorted() {
+    const copy = [...this._objects];
+    if (this._compareFunction != null) copy.sort(this._compareFunction);
+    return copy;
+  }
+
+  splice(start, deleteCount) {
+    return this._objects.splice(start, deleteCount);
+  }
+
+  // Updates an existing object in place, setting the properties specified by
   // `props`. If the object has an updatedAt property, it is set to the current
-  // time.
+  // time. Returns the updated object.
   update(object, props) {
     if (Object.prototype.hasOwnProperty.call(props, 'createdAt')) {
-      // this._objects is sorted by createdAt, so we currently do not support
-      // updates to createdAt.
+      // Objects are ordered in the store in order of creation. If the factory
+      // returns objects with a createdAt property, then the objects should also
+      // be ordered by createdAt. (The order by createdAt should match the
+      // actual order of creation.) Because of that, update() does not support
+      // updating an object's createdAt property.
       throw new Error('createdAt cannot be updated');
     }
     Object.assign(object, props);
@@ -174,23 +144,9 @@ class Store extends Collection {
     }
     return object;
   }
-
-  clear() {
-    this._objects = [];
-    this._createdNew = false;
-  }
-
-  splice(start, deleteCount) { return this._objects.splice(start, deleteCount); }
-
-  sorted() {
-    const copy = [...this._objects];
-    if (this._compareFunction != null) copy.sort(this._compareFunction);
-    return copy;
-  }
-
-  factory() { return this._factory; }
 }
 
+// A view/transformation of a Store
 class View extends Collection {
   constructor(store, transform) {
     super();
@@ -200,29 +156,28 @@ class View extends Collection {
     this._transform = transform.bind(null);
   }
 
-  createPast(count, constraintsOrOptions = undefined) {
-    this._store.createPast(count, constraintsOrOptions);
+  createPast(count, options = undefined) {
+    this._store.createPast(count, options);
     return this;
   }
 
-  createNew(constraintsOrOptions = undefined) {
-    return this._transform(this._store.createNew(constraintsOrOptions));
+  createNew(options = undefined) {
+    return this._transform(this._store.createNew(options));
   }
 
   get size() { return this._store.size; }
   get(index) { return this._transform(this._store.get(index)); }
-  clear() { this._store.clear(); }
-
-  splice(start, deleteCount) {
-    return this._store.splice(start, deleteCount)
-      .map(object => this._transform(object));
-  }
 
   sorted() {
     const sorted = this._store.sorted();
     for (let i = 0; i < sorted.length; i += 1)
       sorted[i] = this._transform(sorted[i]);
     return sorted;
+  }
+
+  splice(start, deleteCount) {
+    return this._store.splice(start, deleteCount)
+      .map(object => this._transform(object));
   }
 }
 
@@ -237,8 +192,6 @@ export const dataStore = (options) => {
 export const view = (store, transform) => new View(store, transform);
 
 export const resetDataStores = () => {
-  for (const store of stores) {
-    store.clear();
-    store.factory().reset();
-  }
+  for (const store of stores)
+    store.reset();
 };
