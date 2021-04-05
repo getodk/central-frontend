e5fe24507d2d8524299a015a7103c63a1a408143
Matthew White <matthew-white@users.noreply.github.com>
2021-04-05 13:25:20 -0400

    Account for null value of __system.edits

--- test/data/submissions.js
+++ test/data/submissions.js
@@ -96,7 +96,7 @@ export const extendedSubmissions = dataStore({
     attachmentsPresent = attachmentsExpected,
     status = null,
     reviewState = null,
-    edits = 0,
+    edits = null,
     deviceId = null,
 
     ...partialOData
