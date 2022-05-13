f972092a30ff3b879356d7f1361d9c2d1105aeab
Matthew White <matthew-white@users.noreply.github.com>
2022-05-13 19:18:50 -0400

    Replace callWait mixin with composable

--- src/components/app.vue
+++ src/components/app.vue
@@ -29,22 +29,18 @@ import VueRouter from 'vue-router';
 import Alert from './alert.vue';
 import Navbar from './navbar.vue';
 
-import callWait from '../mixins/call-wait';
+import useCallWait from '../composables/call-wait';
 import { requestData } from '../store/modules/request';
 import { useSessions } from '../util/session';
 
 export default {
   name: 'App',
   components: { Alert, Navbar },
-  mixins: [callWait()],
   inject: ['alert'],
   setup() {
     useSessions();
-  },
-  data() {
-    return {
-      calls: {}
-    };
+    const { callWait } = useCallWait();
+    return { callWait };
   },
   computed: {
     ...requestData(['centralVersion']),
--- src/composables/call-wait.js
+++ src/composables/call-wait.js
@@ -12,7 +12,7 @@ except according to the terms contained in the LICENSE file.
 
 /*
 Some components need to continuously check for a condition until the condition
-is true. This mixin includes the method callWait() to help do so.
+is true. useCallWait() returns the function callWait() to help do so.
 
 Specify a function to callWait() that checks the condition, returning `true` or
 `false`, then specify a second function that returns the number of milliseconds
@@ -20,11 +20,11 @@ to wait before each check. A component may check for multiple conditions, so you
 must also specify a name for the check. For example:
 
   // Continuously check whether it is the afternoon.
-  this.callWait(
+  callWait(
     'afternoon',
     () => {
       if (DateTime.local().hour < 11) return false;
-      this.alert.info('It is the afternoon!');
+      alert.info('It is the afternoon!');
       return true;
     },
     // Wait a second before each of the first 100 tries, then wait a minute
@@ -39,78 +39,66 @@ condition.
 To cancel an upcoming check, use cancelCall() or cancelCalls(). If an upcoming
 check should be canceled after a route update, it is the component's
 responsibility to do so.
-
-The mixin factory does not take any options.
-
-The component using this mixin must define the following data property:
-
-  - calls. Stores information about the calls. Initialize the property as an
-    empty object. The component using the mixin should not directly mutate this
-    property after defining it.
 */
 
-// @vue/component
-const mixin = {
-  beforeDestroy() {
-    this.cancelCalls();
-  },
-  methods: {
-    callWait(name, call, wait) {
-      if (this.calls[name] != null) throw new Error('name is in use');
+import { onBeforeUnmount } from '@vue/composition-api';
 
-      /* The component might invoke this.cancelCall() during the initial wait
-      before call() is first run. If call() completes an asynchronus action, the
-      component might also invoke this.cancelCall() while the asynchronous
-      action is in progress. Further, after invoking this.cancelCall(), the
-      component may invoke this.callWait() and use the same name as before. That
-      means that there may be multiple actions in progress that are associated
-      with the same name. However, cancel(), defined below, will be unique to
-      each invocation of this.callWait(). */
-      let timeoutId;
-      let canceled = false;
-      const cancel = () => {
-        if (timeoutId != null) clearTimeout(timeoutId);
-        canceled = true;
-      };
-      this.calls[name] = cancel;
+export default () => {
+  const calls = {};
+  const callWait = (name, call, wait) => {
+    if (calls[name] != null) throw new Error('name is in use');
 
-      let count = 0;
-      const f = () => {
-        if (canceled) return;
-        timeoutId = null;
-        Promise.resolve(call())
-          .then(success => {
-            if (canceled) return;
-            if (success) {
-              delete this.calls[name];
-            } else {
-              count += 1;
-              const delay = wait(count);
-              if (delay == null)
-                delete this.calls[name];
-              else
-                timeoutId = setTimeout(f, delay);
-            }
-          })
-          .catch(() => {
-            if (!canceled) delete this.calls[name];
-          });
-      };
-      timeoutId = setTimeout(f, wait(0));
-    },
-    cancelCalls() {
-      for (const cancel of Object.values(this.calls))
-        cancel();
-      this.calls = {};
-    },
-    cancelCall(name) {
-      const cancel = this.calls[name];
-      if (cancel != null) {
-        cancel();
-        delete this.calls[name];
-      }
+    /* The component might invoke cancelCall() during the initial wait before
+    call() is first run. If call() completes an asynchronus action, the
+    component might also invoke cancelCall() while the asynchronous action is in
+    progress. Further, after invoking cancelCall(), the component may invoke
+    callWait() and use the same name as before. That means that there may be
+    multiple actions in progress that are associated with the same name.
+    However, cancel(), defined below, will be unique to each invocation of
+    callWait(). */
+    let timeoutId;
+    let canceled = false;
+    const cancel = () => {
+      if (timeoutId != null) clearTimeout(timeoutId);
+      canceled = true;
+    };
+    calls[name] = cancel;
+
+    let count = 0;
+    const f = () => {
+      if (canceled) return;
+      timeoutId = null;
+      Promise.resolve(call())
+        .then(success => {
+          if (canceled) return;
+          if (success) {
+            delete calls[name];
+          } else {
+            count += 1;
+            const delay = wait(count);
+            if (delay == null)
+              delete calls[name];
+            else
+              timeoutId = setTimeout(f, delay);
+          }
+        })
+        .catch(() => {
+          if (!canceled) delete calls[name];
+        });
+    };
+    timeoutId = setTimeout(f, wait(0));
+  };
+  const cancelCall = (name) => {
+    const cancel = calls[name];
+    if (cancel != null) {
+      cancel();
+      delete calls[name];
     }
-  }
+  };
+  const cancelCalls = () => {
+    for (const name of Object.keys(calls))
+      cancelCall(name);
+  };
+  onBeforeUnmount(cancelCalls);
+  return { callWait, cancelCalls, cancelCall };
 };
-
-export default () => mixin;
