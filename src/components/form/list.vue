87c0a0af606434689f954b1176ec6711d014dac5
Kathleen Tuite <kathleen.tuite@gmail.com>
2022-05-23 16:21:50 -0700

    Moved sort functions to utility file

--- src/components/form/list.vue
+++ src/components/form/list.vue
@@ -46,6 +46,7 @@ import FormSort from './sort.vue';
 import modal from '../../mixins/modal';
 import routes from '../../mixins/routes';
 import { requestData } from '../../store/modules/request';
+import sortFunctions from '../../util/sort';
 
 export default {
   name: 'FormList',
@@ -65,36 +66,6 @@ export default {
   computed: {
     ...requestData(['project', 'forms']),
     sortFunction() {
-      const sortFunctions = {
-        alphabetical: (a, b) => {
-          // sort uses `name` field for both projects and forms
-          // but some forms don't have a name
-          const nameA = a.name != null ? a.name : a.nameOrId();
-          const nameB = b.name != null ? b.name : b.nameOrId();
-          return nameA.localeCompare(nameB);
-        },
-        latest: (a, b) => {
-          const dateA = a.lastSubmission;
-          const dateB = b.lastSubmission;
-          // break tie alphabetically if both lastSub dates are null
-          if (dateA == null && dateB == null) {
-            const nameA = a.name != null ? a.name : a.nameOrId();
-            const nameB = b.name != null ? b.name : b.nameOrId();
-            return nameA.localeCompare(nameB);
-          }
-          // null submission dates should go at the end
-          if (dateA == null)
-            return 1;
-          if (dateB == null)
-            return -1;
-          return new Date(dateB) - new Date(dateA);
-        },
-        newest: (a, b) => {
-          const dateA = a.createdAt;
-          const dateB = b.createdAt;
-          return new Date(dateB) - new Date(dateA);
-        }
-      };
       return sortFunctions[this.sortMode];
     }
   },
