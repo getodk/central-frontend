7514576a6b4732ed015955cbc2d362b5d98280bd
Matthew White <matthew-white@users.noreply.github.com>
2022-11-16 13:44:01 -0500

    Add text about entities to FormDelete

--- test/components/form/delete.spec.js
+++ test/components/form/delete.spec.js
@@ -3,6 +3,14 @@ import FormDelete from '../../../src/components/form/delete.vue';
 import testData from '../../data';
 import { load, mockHttp } from '../../util/http';
 import { mockLogin } from '../../util/session';
+import { mount } from '../../util/lifecycle';
+
+const mountOptions = () => ({
+  props: { state: true },
+  container: {
+    requestData: { form: testData.extendedForms.last() }
+  }
+});
 
 describe('FormDelete', () => {
   beforeEach(mockLogin);
@@ -17,19 +25,32 @@ describe('FormDelete', () => {
       });
   });
 
-  it('implements some standard button things', () =>
-    mockHttp()
-      .mount(FormDelete, {
-        props: { state: true },
-        container: {
-          requestData: { form: testData.extendedForms.createPast(1).last() }
-        }
-      })
+  describe('introductory text', () => {
+    it('shows the correct text if the form creates entities', () => {
+      testData.extendedForms.createPast(1, { entityRelated: true });
+      const modal = mount(FormDelete, mountOptions());
+      const p = modal.findAll('.modal-introduction p');
+      p.length.should.equal(3);
+      p[2].text().should.containEql('Entities');
+    });
+
+    it('shows the correct text if the form does not create entities', () => {
+      testData.extendedForms.createPast(1, { entityRelated: false });
+      const modal = mount(FormDelete, mountOptions());
+      modal.findAll('.modal-introduction p').length.should.equal(2);
+    });
+  });
+
+  it('implements some standard button things', () => {
+    testData.extendedForms.createPast(1);
+    return mockHttp()
+      .mount(FormDelete, mountOptions())
       .testStandardButton({
         button: '.btn-danger',
         disabled: ['.btn-link'],
         modal: true
-      }));
+      });
+  });
 
   describe('after a successful response', () => {
     const del = () => {
