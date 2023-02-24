b3e8b493ff5ba4c6cb35cbcebf9d0df3309fd82e
Kathleen Tuite <kathleen.tuite@gmail.com>
2023-02-23 18:23:47 -0800

    Entity table odata modal tests

--- test/components/dataset/entities.spec.js
+++ test/components/dataset/entities.spec.js
@@ -1,3 +1,6 @@
+import Selectable from '../../../src/components/selectable.vue';
+import OdataAnalyze from '../../../src/components/submission/analyze.vue';
+
 import testData from '../../data';
 import { load } from '../../util/http';
 import { mockLogin } from '../../util/session';
@@ -17,5 +20,26 @@ describe('DatasetEntities', () => {
     button.text().should.equal('Download 10 Entities');
     const { href } = button.attributes();
     href.should.equal('/v1/projects/1/datasets/trees/entities.csv');
+  });
+
+  describe('OData modal', () => {
+    beforeEach(() => {
+      testData.extendedDatasets.createPast(1, { name: 'trees' });
+    });
+
+    it('toggles the modal', () =>
+      load('/projects/1/datasets/trees/entities', { root: false }).testModalToggles({
+        modal: OdataAnalyze,
+        show: '#odata-data-access-analyze-button',
+        hide: '.btn-primary'
+      }));
+
+    it('shows the correct URL from entities page', async () => {
+      const component = await load('/projects/1/datasets/trees/entities', {
+        root: false
+      });
+      const text = component.getComponent(Selectable).text();
+      text.should.equal(`${window.location.origin}/v1/projects/1/datasets/trees.svc`);
+    });
   });
 });
