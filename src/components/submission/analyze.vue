d07987a7aca2db9ea7886b8df5d0a77aa169b4c1
Yaw Anokwa <yanokwa@users.noreply.github.com>
2023-02-27 09:13:21 -0800

    Shorter message to prevent wrapping (#744)

--- src/components/submission/analyze.vue
+++ src/components/submission/analyze.vue
@@ -21,13 +21,13 @@ except according to the terms contained in the LICENSE file.
               <a href="https://powerbi.microsoft.com/en-us/" target="_blank" rel="noopener">Power BI</a>
             </template>
             <template #excel>
-              <a href="https://www.microsoft.com/en-us/microsoft-365/excel" target="_blank" rel="noopener">Excel</a>
+              <a href="https://www.microsoft.com/en-us/microsoft-365/excel/" target="_blank" rel="noopener">Excel</a>
             </template>
             <template #python>
               <a href="https://www.python.org/" target="_blank" rel="noopener">Python</a>
             </template>
             <template #r>
-              <a href="https://www.r-project.org" target="_blank" rel="noopener">R</a>
+              <a href="https://www.r-project.org/" target="_blank" rel="noopener">R</a>
             </template>
           </i18n-t>
           <p>{{ $t('introduction[1]') }}</p>
@@ -56,7 +56,7 @@ except according to the terms contained in the LICENSE file.
             <a href="https://support.microsoft.com/en-us/office/about-power-query-in-excel-7104fbee-9e62-4cb9-a02e-5bfb1a6c536a" target="_blank" rel="noopener">{{ $t('help.microsoft.pageForExcel') }}</a>
           </template>
           <template #pageForPowerBi>
-            <a href="https://learn.microsoft.com/en-us/power-bi/connect-data/desktop-connect-odata" target="_blank" rel="noopener">{{ $t('help.microsoft.pageForPowerBi') }}</a>
+            <a href="https://learn.microsoft.com/en-us/power-bi/connect-data/desktop-connect-odata/" target="_blank" rel="noopener">{{ $t('help.microsoft.pageForPowerBi') }}</a>
           </template>
         </i18n-t>
         <i18n-t v-else-if="tool === 'python'" tag="p" keypath="help.python">
@@ -71,7 +71,7 @@ except according to the terms contained in the LICENSE file.
         </i18n-t>
         <i18n-t v-else-if="tool === 'other'" tag="p" keypath="help.other.full">
           <template #article>
-            <a href="https://odkcentral.docs.apiary.io/#reference/odata-endpoints" target="_blank" rel="noopener">{{ $t('help.other.article') }}</a>
+            <a href="https://odkcentral.docs.apiary.io/#reference/odata-endpoints/" target="_blank" rel="noopener">{{ $t('help.other.article') }}</a>
           </template>
         </i18n-t>
       </div>
@@ -190,7 +190,7 @@ export default {
         "pageForPowerBi": "this page"
       },
       // {pyODK} is a link. Its text is "pyODK".
-      "python": "To connect to Central from Python, we recommend {pyODK}. pyODK is the official Python client for Central and it makes common data analysis and workflow automation tasks simple.",
+      "python": "To connect to Central from Python, we recommend {pyODK}. pyODK is the official Python client for Central. It simplifies data analysis and workflow automation.",
       // {ruODK} is a link. Its text is "ruODK".
       "r": "To connect to Central from R, we recommend {ruODK}. ruODK is developed and supported by ODK community members.",
       "other": {
