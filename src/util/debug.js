c50812a0a31f76f8940e620dc40bf2a230c2e908
Matthew White <matthew-white@users.noreply.github.com>
2023-04-03 20:32:09 -0400

    Add additional comments

--- src/util/debug.js
+++ src/util/debug.js
@@ -71,8 +71,20 @@ class RenderTimer {
   }
 }
 
-// useRenderTimer() is a composable. It returns a timer object that can help
-// measure performance around rendering.
+/*
+useRenderTimer() is a composable. It returns a timer object that can help
+measure performance around rendering. After the component is mounted and after
+each component update, the composable logs the duration of the render. If you
+turn the timer on by calling its on() method, it will also store the duration of
+each render. Turn the timer off to stop storing durations. Call the summarize()
+method to calculate summary statistics about the durations stored on the timer.
+
+Usually Vue devtools are a more convenient way to learn about the performance of
+a component. However, useRenderTimer() can be helpful when combined with other
+logging or as a way to calculate summary statistics about render times. You may
+find it convenient to temporarily assign the timer to `window` to make it easy
+to access from the console.
+*/
 export const useRenderTimer = () => {
   const timer = new RenderTimer();
 
