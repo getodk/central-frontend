535fbb6dfac7d9f423f2b56ee537078667a52487
Matthew White <matthew-white@users.noreply.github.com>
2023-04-06 23:47:29 -0400

    Improve output to file

--- test/run.sh
+++ test/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -eu
 set -o pipefail
 
-NODE_ENV=test karma start | tee test/output.txt
-if grep -F -e 'WARN LOG:' -e 'WARN [web-server]:' -C5 test/output.txt; then
+output=$(mktemp)
+trap 'rm -- "$output"' EXIT
+NODE_ENV=test karma start | tee "$output"
+if grep -F -e 'WARN LOG:' -e 'WARN [web-server]:' -C5 "$output"; then
 	echo
 	echo 'All tests passed, but there were warnings: see above.'
 	exit 1
 fi
-rm test/output.txt
