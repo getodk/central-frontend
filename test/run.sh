9e1e5d7f0501f9765bdd6531df097f947db7a581
Matthew White <matthew-white@users.noreply.github.com>
2023-04-10 15:50:11 -0400

    Check for Sass warnings when running tests

--- test/run.sh
+++ test/run.sh
@@ -4,7 +4,9 @@ set -o pipefail
 output=$(mktemp)
 trap 'rm -- "$output"' EXIT
 NODE_ENV=test karma start | tee "$output"
-if grep -F -e 'WARN LOG:' -e 'WARN [web-server]:' -C5 "$output"; then
+# Search for: warnings from console.warn(), including Vue warnings; Sass
+# warnings; and warnings from Karma.
+if grep -F -e 'WARN LOG:' -e 'Module Warning' -e 'WARN [web-server]:' -C5 "$output"; then
 	echo
 	echo 'All tests passed, but there were warnings: see above.'
 	exit 1
