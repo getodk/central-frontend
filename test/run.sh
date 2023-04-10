8cd1b22cc12df4a1e444d69699996cd8e5b4df52
Matthew White <matthew-white@users.noreply.github.com>
2023-04-10 16:23:06 -0400

    Reset text format after displaying test warnings

--- test/run.sh
+++ test/run.sh
@@ -7,6 +7,8 @@ NODE_ENV=test karma start | tee "$output"
 # Search for: warnings from console.warn(), including Vue warnings; Sass
 # warnings; and warnings from Karma.
 if grep -F -e 'WARN LOG:' -e 'Module Warning' -e 'WARN [web-server]:' -C5 "$output"; then
+	# Reset the text format in case the search results contained formatted text.
+	tput sgr0
 	echo
 	echo 'All tests passed, but there were warnings: see above.'
 	exit 1
