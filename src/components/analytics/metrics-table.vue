da77ddf0c746bc340f1006cb77201736435f09f1
Matthew White <matthew-white@users.noreply.github.com>
2023-05-25 22:02:28 -0400

    Update usage information metrics for v2023.3 (#809)
    See getodk/central#410.

--- src/components/analytics/metrics-table.vue
+++ src/components/analytics/metrics-table.vue
@@ -109,7 +109,8 @@ export default {
       "num_creation_forms": "Number of Forms that update the Dataset",
       "num_followup_forms": "Number of Forms that use the Dataset",
       "num_entities": "Number of Entities in the Dataset",
-      "num_failed_entities": "Number of Entity creation errors"
+      "num_failed_entities": "Number of Entity creation errors",
+      "num_entity_updates": "Number of Entity updates"
     }
   }
 }
