8fb2c92442a442e198383552f1730419481bc15f
alxndrsn <alxndrsn>
2023-07-26 13:20:00 +0000

    set oidcEnabled=false by default

--- src/config.js
+++ src/config.js
@@ -16,5 +16,5 @@ export default {
     title: null,
     body: null
   },
-  oidcEnabled: true, // TODO set false or remove before merge - this needs to be supplied by the server (probably)!
+  oidcEnabled: !!process.env.ENABLE_OIDC, // TODO remove before merge - this needs to be supplied by the backend
 };
