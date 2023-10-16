740551405c0bec79f2a8b7021b60bf5c6ae9745b
eyelidlessness <gnosis@gmail.com>
2023-10-15 23:11:30 -0700

    Fix sorting in jsdom

--- packages/odk-xpath/src/lib/dom/sort.ts
+++ packages/odk-xpath/src/lib/dom/sort.ts
@@ -1,3 +1,5 @@
+import type { MaybeAttrNode } from './types';
+
 const { DOCUMENT_POSITION_FOLLOWING, DOCUMENT_POSITION_PRECEDING } = Node;
 
 export const sortDocumentOrder = (nodes: Iterable<Node>) => {
@@ -9,6 +11,17 @@ export const sortDocumentOrder = (nodes: Iterable<Node>) => {
 
 	return array.sort((a, b) => {
 		const compared = b.compareDocumentPosition(a);
+
+		// Specific to jsdom(?)
+		if (compared === 0 && a !== b) {
+			if ((a as MaybeAttrNode).ownerElement === b) {
+				return 1;
+			}
+
+			if (a === (b as MaybeAttrNode).ownerElement) {
+				return -1;
+			}
+		}
 
 		if (compared & DOCUMENT_POSITION_FOLLOWING) {
 			return 1;
