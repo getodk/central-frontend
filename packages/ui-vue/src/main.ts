07d298d5b7905a2e0d21363b75e0afc0530f716b
Sadiq Khoja <sadiqkhoja@gmail.com>
2024-03-11 18:50:20 -0400

    Add PrimeVue, PrimeFlex, PrimeVue-Sass-Theme

--- packages/ui-vue/src/main.ts
+++ packages/ui-vue/src/main.ts
@@ -1,11 +1,17 @@
 import { createApp } from 'vue';
-import App from './App.vue';
-import './style.css';
+import type { Component } from 'vue';
 
-createApp(
-	// ESLint can't agree with itself whether or not this module exists and has
-	// known types.
-	//
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-argument
-	App
-).mount('#app');
+import PrimeVue from 'primevue/config';
+import App from './App.vue';
+
+// Due to vite#11012, Roboto font files are manually copied in public/fonts folder.
+import './themes/2024-light/theme.scss';
+
+import './assets/css/style.css';
+
+// TODO: Purge it - postcss-purgecss
+import 'primeflex/primeflex.css';
+
+const app = createApp(App as Component);
+app.use(PrimeVue, { ripple: false }); // Collect has no ripple
+app.mount('#app');
