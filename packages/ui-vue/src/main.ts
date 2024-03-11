ac90e6b3c0e29ab29e8e4d9a98576f6baaf94694
Sadiq Khoja <sadiqkhoja@gmail.com>
2024-03-11 18:51:15 -0400

    Add icomoon files with required Material icons

--- packages/ui-vue/src/main.ts
+++ packages/ui-vue/src/main.ts
@@ -7,6 +7,7 @@ import App from './App.vue';
 // Due to vite#11012, Roboto font files are manually copied in public/fonts folder.
 import './themes/2024-light/theme.scss';
 
+import './assets/css/icomoon.css';
 import './assets/css/style.css';
 
 // TODO: Purge it - postcss-purgecss
