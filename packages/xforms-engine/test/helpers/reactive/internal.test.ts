getodk/web-forms

Minimal internal reactivity implementation, sutiable for testing…
interaction with the engine/client API’s OpaqueReactiveObjectFactory.

packages/xforms-engine/test/helpers/reactive/internal.test.ts
@@ -1,0 +1,207 @@
+import { describe, expect, it } from 'vitest';
+import { reactiveScope } from './internal.ts';
+
+describe('Internal minimal reactivity implementation (itself currently for use in tests)', () => {
+	it('creates an object with the same shape and property values', () => {
+		expect.assertions(1);
+
+		reactiveScope(({ mutable }) => {
+			const object = mutable({
+				a: 'a',
+				b: 1,
+			});
+
+			expect(object).toEqual({
+				a: 'a',
+				b: 1,
+			});
+		});
+	});
+
+	it('subscribes to mutations when reading a property within an effect', () => {
+		expect.assertions(2);
+
+		reactiveScope(({ mutable, effect }) => {
+			const object = mutable({
+				a: 'a',
+				b: 1,
+			});
+
+			let effectReactions = 0;
+
+			effect(() => {
+				effectReactions += 1;
+
+				object.a;
+			});
+
+			expect(effectReactions).toBe(1);
+
+			object.a = 'c';
+
+			expect(effectReactions).toBe(2);
+		});
+	});
+
+	it('does not subscribe to mutations affecting properties unread within an effect', () => {
+		expect.assertions(3);
+
+		reactiveScope(({ mutable, effect }) => {
+			const object = mutable({
+				a: 'a',
+				b: 1,
+			});
+
+			let effectReactions = 0;
+
+			effect(() => {
+				effectReactions += 1;
+
+				object.a;
+			});
+
+			expect(effectReactions).toBe(1);
+
+			object.b = 2;
+
+			expect(effectReactions).toBe(1);
+
+			object.b = 3;
+
+			expect(effectReactions).toBe(1);
+		});
+	});
+
+	it('does not subscribe to mutations independently across effects, depending on which property values are read', () => {
+		expect.assertions(3);
+
+		reactiveScope(({ mutable, effect }) => {
+			const object = mutable({
+				a: 'a',
+				b: 1,
+			});
+
+			const effectReactions = {
+				a: 0,
+				b: 0,
+			};
+
+			effect(() => {
+				effectReactions.a += 1;
+
+				object.a;
+			});
+
+			effect(() => {
+				effectReactions.b += 1;
+
+				object.b;
+			});
+
+			expect(effectReactions).toStrictEqual({
+				a: 1,
+				b: 1,
+			});
+
+			object.b = 2;
+			object.b = 3;
+
+			expect(effectReactions).toStrictEqual({
+				a: 1,
+				b: 3,
+			});
+
+			object.a = 'b';
+
+			expect(effectReactions).toStrictEqual({
+				a: 2,
+				b: 3,
+			});
+		});
+	});
+
+	it('subscribes to computed mutations when reading the computation within an effect', () => {
+		expect.assertions(1);
+
+		reactiveScope(({ mutable, computed, effect }) => {
+			const object = mutable({
+				a: 'a',
+				b: 1,
+			});
+
+			const serialized = computed(() => {
+				return `a: "${object.a}", b: ${object.b}`;
+			});
+
+			const tracked: string[] = [];
+
+			effect(() => {
+				tracked.push(serialized());
+			});
+
+			object.a = 'b';
+			object.b = 2;
+			object.a = 'c';
+
+			expect(tracked).toStrictEqual([
+				'a: "a", b: 1',
+				'a: "b", b: 1',
+				'a: "b", b: 2',
+				'a: "c", b: 2',
+			]);
+		});
+	});
+
+	it('preserves access to getters in the provided object', () => {
+		expect.assertions(1);
+
+		reactiveScope(({ mutable }) => {
+			const object = mutable({
+				a: 'a',
+				get b() {
+					return `this is b, the current value of a is "${this.a}"`;
+				},
+			});
+
+			expect(object).toEqual({
+				a: 'a',
+				b: 'this is b, the current value of a is "a"',
+			});
+		});
+	});
+
+	it('subscribes to mutations affecting a getter which is read within an effect', () => {
+		expect.assertions(2);
+
+		reactiveScope(({ mutable, effect }) => {
+			const object = mutable({
+				a: 'a',
+				get b() {
+					return `this is b, the current value of a is "${this.a}"`;
+				},
+			});
+
+			let effectReactions = 0;
+
+			effect(() => {
+				effectReactions += 1;
+
+				object.b;
+			});
+
+			expect(effectReactions).toBe(1);
+
+			object.a = 'z';
+
+			expect(effectReactions).toBe(2);
+		});
+	});
+
+	it("returns an arbitrary value produced by the reactive scope's callback", () => {
+		const result = reactiveScope(() => {
+			return 'I am arbitrary';
+		});
+
+		expect(result).toBe('I am arbitrary');
+	});
+});

packages/xforms-engine/test/helpers/reactive/internal.ts
@@ -1,0 +1,290 @@
+/**
+ * @module - Provides a reactivity implementation with the minimal functionality
+ * needed for satisfying **and testing** the engine's use of
+ * {@link OpaqueReactiveObjectFactory}.
+ *
+ * The implementation is partially based on
+ * {@link https://dev.to/ryansolid/building-a-reactive-library-from-scratch-1i0p},
+ * a reactive internals tutorial by the creator of Solid. As such, it bears some
+ * resemblance to the internal design of Solid's own reactive primitives. But it
+ * intentionally differs in a few ways:
+ *
+ * - It doesn't expose a reactive atomic value primitive (e.g. a "signal"), as
+ *   we don't use this in our engine/client interface.
+ *
+ * - That primitive is used to compose others internally, however. Its internal
+ *   shape is slightly different, but it preserves Solid's intentional
+ *   read/write segregation design. (Not that it matters from the outside, but
+ *   it should help with reasoning about the internal compositions.)
+ *
+ * - The primitives it does expose have slightly different names. This is mostly
+ *   to avoid confusion if/where these APIs might coexist with Solid's APIs in
+ *   the same module, and to avoid auto-import mistakes so long as we continue
+ *   to use Solid for internal reactivity concerns.
+ *
+ * @todo it's entirely possible this limited reactive API is (or becomes)
+ * actually suitable for engine-internal use cases, and we might consider
+ * dropping dependencies for such internal use.
+ */
+
+import type { Primitive } from '@odk-web-forms/common/types/Primitive.ts';
+import type { OpaqueReactiveObjectFactory } from '../../../src/client/OpaqueReactiveObjectFactory.ts';
+
+/**
+ * A value assigned to a {@link MutableState} property.
+ *
+ * @todo this type is intentionally limited to focus on our use case for the
+ * engine/client reactive bridge, where we have intentionally designed for a
+ * shallow representation of state. Notably, this type doesn't **yet** address
+ * the use case for providing a shallow mapping for dynamic children (i.e.
+ * repeat instances). It's defined as a distinct type anticipating that will be
+ * implemented in a subsequent commit.
+ */
+type MutableStateValue = Primitive;
+
+// A rare intentionally mutable object appears!
+export type MutableState = Record<string, MutableStateValue>;
+
+/**
+ * An intentionally restricted subset of {@link OpaqueReactiveObjectFactory}.
+ *
+ * - The mutable object's shape is deliberately fixed on creation, i.e.
+ *   properties cannot be added or removed.
+ * - @see {@link MutableStateValue} for detail about restrictions on individual
+ *   property values.
+ */
+type MutableStateFactory = <T extends MutableState>(object: T) => T;
+
+type Thunk<T> = () => T;
+
+/**
+ * Defines a thunk producing a computed value, which may be produced from one or
+ * more reactive values. Like an {@link AtomicState} value, calling this thunk
+ * in a reactive context (like an {@link effect}) acts as a subscription; writes
+ * to any underlying reactive values used in this computation also trigger
+ * reactive updates where this thunk is called.
+ *
+ * @todo This is closely related to what Solid calls a "memo", but there is
+ * presently no memoization mechanism in place to restrict downstream reactions.
+ * But it does utilize {@link AtomicState} as an intermediate store, so we'd get
+ * some notion of memoization here "for free" if we did elect to implement
+ * "updated value" checking logic there.
+ */
+export type DefineComputation = <T>(computeFn: Thunk<T>) => Thunk<T>;
+
+/**
+ * Defines a reactive subscription context. Runs synchronously when defined.
+ * Reading reactive values within `effectFn` establishes a subscription, causing
+ * `effectFn` to be re-run for writes to those reactive values.
+ *
+ * @todo effects may run more frequently than we'd prefer in real world usage,
+ * both due to reasoning discussed in {@link Write} and because there is
+ * currently no batching mechanism for multiple synchronous writes.
+ */
+export type DefineEffect = (effectFn: VoidFunction) => void;
+
+/**
+ * A reactive scope isolating reactive subscriptions within a
+ * {@link ReactiveTestScopeCallback}.
+ *
+ * This is roughly a subset of Solid's concept of a
+ * {@link https://www.solidjs.com/docs/latest/api#createroot | reactive root},
+ * with these limitations/distinctions:
+ *
+ * - Nesting is not supported
+ * - Contexts cannot be referenced
+ * - Disposal is implicit when the callback exits\*
+ * - Each reactive primitive is inherently bound to the context of the {@link ReactiveTestContextCallback}; this API is intentionally non-global.
+ *
+ * @todo \* Disposal is _so implicit_ that nothing is actually done to clean
+ * anything up. Since we're initially targeting use in tests, we can generally
+ * just rely on the GC where it's effective, ignore memory leaks as ephemeral,
+ * and generally try to avoid exposing reactive stuff outside the reactive test
+ * scope's callback (e.g. either with outer scope bindings, or the reactive
+ * scope's own return value). Importantly, since there's no explicit disposal
+ * mechanism, we should be careful with any async/timer logic within a reactive
+ * test scope.
+ */
+interface ReactiveTestScope {
+	readonly mutable: MutableStateFactory;
+	readonly computed: DefineComputation;
+	readonly effect: DefineEffect;
+}
+
+/**
+ * An arbitrary function which is executed with a {@link ReactiveTestScope}.
+ * Since this reactive implementation is presently targeted for test use cases,
+ * it's anticipated that the callback will be either used to perform a test's
+ * logic directly, or to perform some subset of that test logic like setup.
+ *
+ * The function may return a value of any type, which will in turn be returned
+ * from {@link reactiveScope} where it is called. As discussed in
+ * {@link ReactiveTestScope}, care should be taken not to break the scope of
+ * reactivity itself.
+ */
+type ReactiveTestScopeCallback<T> = (context: ReactiveTestScope) => T;
+
+/**
+ * Runs an arbitrary {@link ReactiveTestContextCallback} within a
+ * {@link ReactiveTestScope}, and returns the callback's value. See docs for
+ * both for additional detail and considerations.
+ */
+export const reactiveScope = <Result>(callback: ReactiveTestScopeCallback<Result>): Result => {
+	interface Dependency {
+		readonly subscribers: Set<Subscriber>;
+	}
+
+	interface Subscriber {
+		readonly execute: VoidFunction;
+		readonly dependencies: Set<Dependency>;
+	}
+
+	const subscribers: Subscriber[] = [];
+
+	/**
+	 * A thunk which produces a (potentially) reactive value when called, and which
+	 * establishes a subscription when called within a reactive context.
+	 */
+	type ReadAtom<T> = Thunk<T>;
+
+	/**
+	 * A function used to update an atomic reactive value, and which updates
+	 * reactive subscriptions when called with an updated value.
+	 *
+	 * @todo "updated value" is not currently checked
+	 * @todo we don't currently accept a callback with the current value, but it
+	 * might be a good idea if we expand usage
+	 */
+	type WriteAtom<T> = (value: T) => T;
+
+	/**
+	 * Provides both read and write access to an atomic reactive value (e.g. a
+	 * "signal"). Note that this is not the interface for mutables.
+	 */
+	interface Atom<T> {
+		readonly read: ReadAtom<T>;
+		readonly write: WriteAtom<T>;
+	}
+
+	/**
+	 * Produces an atomic reactive value (e.g. a "signal").
+	 */
+	type DefineAtom = <T>(initialValue: T) => Atom<T>;
+
+	const cleanupSubscriber = (running: Subscriber) => {
+		const { dependencies } = running;
+
+		for (const dependency of dependencies) {
+			dependency.subscribers.delete(running);
+		}
+
+		dependencies.clear();
+	};
+
+	class AtomicState<T> implements Atom<T>, Dependency {
+		readonly read: ReadAtom<T>;
+		readonly write: WriteAtom<T>;
+		readonly subscribers = new Set<Subscriber>();
+
+		constructor(value: T) {
+			this.read = (): T => {
+				const running = subscribers[subscribers.length - 1];
+
+				if (running) {
+					this.subscribers.add(running);
+
+					running.dependencies.add(this);
+				}
+
+				return value;
+			};
+
+			this.write = (nextValue: T) => {
+				/**
+				 * @see {@link WriteAtom}, this is likely where we'd think about checking
+				 * whether a value change has actually occurred to determine whether
+				 * any subscriptions should be notified.
+				 */
+				value = nextValue;
+
+				for (const subscriber of [...this.subscribers]) {
+					subscriber.execute();
+				}
+
+				return value;
+			};
+		}
+	}
+
+	const atom: DefineAtom = (value) => {
+		return new AtomicState(value);
+	};
+
+	const computed: DefineComputation = <T>(fn: Thunk<T>): Thunk<T> => {
+		const { read, write } = atom<T>(undefined!);
+
+		effect(() => write(fn()));
+
+		return read;
+	};
+
+	const effect: DefineEffect = (effectFn: VoidFunction) => {
+		const execute = () => {
+			cleanupSubscriber(running);
+
+			subscribers.push(running);
+
+			try {
+				effectFn();
+			} finally {
+				subscribers.pop();
+			}
+		};
+
+		const running: Subscriber = {
+			execute,
+			dependencies: new Set(),
+		};
+
+		execute();
+	};
+
+	const mutable = <T extends MutableState>(object: T): T => {
+		// Creating a null-property object ensures we're *only* creating reactive
+		// properties for the explicitly defined members of the input object.
+		const reactiveObject = Object.create(null) as T;
+
+		// Map each "normal" (read/writable) property to a computed get/set property
+		// backed by an Atom. For other properties:
+		//
+		// - Existing computed properties are preserved exactly as they're defined.
+		// - Inherited (e.g. by prototype/class extension) properties are omitted.
+		Object.entries(Object.getOwnPropertyDescriptors(object)).forEach(([key, inputDescriptor]) => {
+			if (inputDescriptor.writable) {
+				const { read, write } = atom(inputDescriptor.value);
+
+				Object.defineProperty(reactiveObject, key, {
+					configurable: false,
+					enumerable: true,
+					get: read,
+					set: write,
+				});
+			} else {
+				Object.defineProperty(reactiveObject, key, inputDescriptor);
+			}
+		});
+
+		return reactiveObject;
+	};
+
+	// Having now established the API available to the reactive scope, we execute
+	// the provided callback with that API. We would likely perform any cleanup
+	// after this call.
+	const result = callback({
+		mutable,
+		computed,
+		effect,
+	});
+
+	return result;
+};

getodk/central-frontend

packages/xforms-engine/test/helpers/reactive/internal.test.ts
@@ -1,0 +1,207 @@
+import { describe, expect, it } from 'vitest';
+import { reactiveTestScope } from './internal.ts';
+
+describe('Internal minimal reactivity implementation (itself currently for use in tests)', () => {
+	it('creates an object with the same shape and property values', () => {
+		expect.assertions(1);
+
+		reactiveTestScope(({ mutable }) => {
+			const object = mutable({
+				a: 'a',
+				b: 1,
+			});
+
+			expect(object).toEqual({
+				a: 'a',
+				b: 1,
+			});
+		});
+	});
+
+	it('subscribes to mutations when reading a property within an effect', () => {
+		expect.assertions(2);
+
+		reactiveTestScope(({ mutable, effect }) => {
+			const object = mutable({
+				a: 'a',
+				b: 1,
+			});
+
+			let effectReactions = 0;
+
+			effect(() => {
+				effectReactions += 1;
+
+				object.a;
+			});
+
+			expect(effectReactions).toBe(1);
+
+			object.a = 'c';
+
+			expect(effectReactions).toBe(2);
+		});
+	});
+
+	it('does not subscribe to mutations affecting properties unread within an effect', () => {
+		expect.assertions(3);
+
+		reactiveTestScope(({ mutable, effect }) => {
+			const object = mutable({
+				a: 'a',
+				b: 1,
+			});
+
+			let effectReactions = 0;
+
+			effect(() => {
+				effectReactions += 1;
+
+				object.a;
+			});
+
+			expect(effectReactions).toBe(1);
+
+			object.b = 2;
+
+			expect(effectReactions).toBe(1);
+
+			object.b = 3;
+
+			expect(effectReactions).toBe(1);
+		});
+	});
+
+	it('does not subscribe to mutations independently across effects, depending on which property values are read', () => {
+		expect.assertions(3);
+
+		reactiveTestScope(({ mutable, effect }) => {
+			const object = mutable({
+				a: 'a',
+				b: 1,
+			});
+
+			const effectReactions = {
+				a: 0,
+				b: 0,
+			};
+
+			effect(() => {
+				effectReactions.a += 1;
+
+				object.a;
+			});
+
+			effect(() => {
+				effectReactions.b += 1;
+
+				object.b;
+			});
+
+			expect(effectReactions).toStrictEqual({
+				a: 1,
+				b: 1,
+			});
+
+			object.b = 2;
+			object.b = 3;
+
+			expect(effectReactions).toStrictEqual({
+				a: 1,
+				b: 3,
+			});
+
+			object.a = 'b';
+
+			expect(effectReactions).toStrictEqual({
+				a: 2,
+				b: 3,
+			});
+		});
+	});
+
+	it('subscribes to computed mutations when reading the computation within an effect', () => {
+		expect.assertions(1);
+
+		reactiveTestScope(({ mutable, computed, effect }) => {
+			const object = mutable({
+				a: 'a',
+				b: 1,
+			});
+
+			const serialized = computed(() => {
+				return `a: "${object.a}", b: ${object.b}`;
+			});
+
+			const tracked: string[] = [];
+
+			effect(() => {
+				tracked.push(serialized());
+			});
+
+			object.a = 'b';
+			object.b = 2;
+			object.a = 'c';
+
+			expect(tracked).toStrictEqual([
+				'a: "a", b: 1',
+				'a: "b", b: 1',
+				'a: "b", b: 2',
+				'a: "c", b: 2',
+			]);
+		});
+	});
+
+	it('preserves access to getters in the provided object', () => {
+		expect.assertions(1);
+
+		reactiveTestScope(({ mutable }) => {
+			const object = mutable({
+				a: 'a',
+				get b() {
+					return `this is b, the current value of a is "${this.a}"`;
+				},
+			});
+
+			expect(object).toEqual({
+				a: 'a',
+				b: 'this is b, the current value of a is "a"',
+			});
+		});
+	});
+
+	it('subscribes to mutations affecting a getter which is read within an effect', () => {
+		expect.assertions(2);
+
+		reactiveTestScope(({ mutable, effect }) => {
+			const object = mutable({
+				a: 'a',
+				get b() {
+					return `this is b, the current value of a is "${this.a}"`;
+				},
+			});
+
+			let effectReactions = 0;
+
+			effect(() => {
+				effectReactions += 1;
+
+				object.b;
+			});
+
+			expect(effectReactions).toBe(1);
+
+			object.a = 'z';
+
+			expect(effectReactions).toBe(2);
+		});
+	});
+
+	it("returns an arbitrary value produced by the reactive scope's callback", () => {
+		const result = reactiveTestScope(() => {
+			return 'I am arbitrary';
+		});
+
+		expect(result).toBe('I am arbitrary');
+	});
+});

packages/xforms-engine/test/helpers/reactive/internal.ts
@@ -1,0 +1,293 @@
+/**
+ * @module - Provides a reactivity implementation with the minimal functionality
+ * needed for satisfying **and testing** the engine's use of
+ * {@link OpaqueReactiveObjectFactory}.
+ *
+ * The implementation is partially based on
+ * {@link https://dev.to/ryansolid/building-a-reactive-library-from-scratch-1i0p},
+ * a reactive internals tutorial by the creator of Solid. As such, it bears some
+ * resemblance to the internal design of Solid's own reactive primitives. But it
+ * intentionally differs in a few ways:
+ *
+ * - It doesn't expose a reactive atomic value primitive (e.g. a "signal"), as
+ *   we don't use this in our engine/client interface.
+ *
+ * - That primitive is used to compose others internally, however. Its internal
+ *   shape is slightly different, but it preserves Solid's intentional
+ *   read/write segregation design. (Not that it matters from the outside, but
+ *   it should help with reasoning about the internal compositions.)
+ *
+ * - The primitives it does expose have slightly different names. This is mostly
+ *   to avoid confusion if/where these APIs might coexist with Solid's APIs in
+ *   the same module, and to avoid auto-import mistakes so long as we continue
+ *   to use Solid for internal reactivity concerns.
+ *
+ * @todo it's entirely possible this limited reactive API is (or becomes)
+ * actually suitable for engine-internal use cases, and we might consider
+ * dropping dependencies for such internal use.
+ */
+
+import type { Primitive } from '@odk-web-forms/common/types/Primitive.ts';
+import type { OpaqueReactiveObjectFactory } from '../../../src/client/OpaqueReactiveObjectFactory.ts';
+
+/**
+ * A value assigned to a {@link MutableState} property.
+ *
+ * @todo this type is intentionally limited to focus on our use case for the
+ * engine/client reactive bridge, where we have intentionally designed for a
+ * shallow representation of state. Notably, this type doesn't **yet** address
+ * the use case for providing a shallow mapping for dynamic children (i.e.
+ * repeat instances). It's defined as a distinct type anticipating that will be
+ * implemented in a subsequent commit.
+ */
+type MutableStateValue = Primitive;
+
+// A rare intentionally mutable object appears!
+export type MutableState = Record<string, MutableStateValue>;
+
+/**
+ * An intentionally restricted subset of {@link OpaqueReactiveObjectFactory}.
+ *
+ * - The mutable object's shape is deliberately fixed on creation, i.e.
+ *   properties cannot be added or removed.
+ * - @see {@link MutableStateValue} for detail about restrictions on individual
+ *   property values.
+ */
+type MutableStateFactory = <T extends MutableState>(object: T) => T;
+
+type Thunk<T> = () => T;
+
+/**
+ * Defines a thunk producing a computed value, which may be produced from one or
+ * more reactive values. Like an {@link AtomicState} value, calling this thunk
+ * in a reactive context (like an {@link effect}) acts as a subscription; writes
+ * to any underlying reactive values used in this computation also trigger
+ * reactive updates where this thunk is called.
+ *
+ * @todo This is closely related to what Solid calls a "memo", but there is
+ * presently no memoization mechanism in place to restrict downstream reactions.
+ * But it does utilize {@link AtomicState} as an intermediate store, so we'd get
+ * some notion of memoization here "for free" if we did elect to implement
+ * "updated value" checking logic there.
+ */
+export type DefineComputation = <T>(computeFn: Thunk<T>) => Thunk<T>;
+
+/**
+ * Defines a reactive subscription context. Runs immediately upon definition.
+ * Reading reactive values within `effectFn` establishes a subscription, causing
+ * `effectFn` to be re-run for writes to those reactive values.
+ *
+ * @todo effects may run more frequently than we'd prefer in real world usage,
+ * both due to reasoning discussed in {@link Write} and because there is
+ * currently no batching mechanism for multiple synchronous writes. As such (and
+ * despite what the name might imply), the current `effect` implementation most
+ * closely resembles Solid's
+ * {@link https://docs.solidjs.com/reference/secondary-primitives/create-computed | `createComputed`}.
+ */
+export type DefineEffect = (effectFn: VoidFunction) => void;
+
+/**
+ * A reactive scope isolating reactive subscriptions within a
+ * {@link ReactiveTestScopeCallback}.
+ *
+ * This is roughly a subset of Solid's concept of a
+ * {@link https://www.solidjs.com/docs/latest/api#createroot | reactive root},
+ * with these limitations/distinctions:
+ *
+ * - Nesting is not supported
+ * - Contexts cannot be referenced
+ * - Disposal is implicit when the callback exits\*
+ * - Each reactive primitive is inherently bound to the context of the {@link ReactiveTestContextCallback}; this API is intentionally non-global.
+ *
+ * @todo \* Disposal is _so implicit_ that nothing is actually done to clean
+ * anything up. Since we're initially targeting use in tests, we can generally
+ * just rely on the GC where it's effective, ignore memory leaks as ephemeral,
+ * and generally try to avoid exposing reactive stuff outside the reactive test
+ * scope's callback (e.g. either with outer scope bindings, or the reactive
+ * scope's own return value). Importantly, since there's no explicit disposal
+ * mechanism, we should be careful with any async/timer logic within a reactive
+ * test scope.
+ */
+interface ReactiveTestScope {
+	readonly mutable: MutableStateFactory;
+	readonly computed: DefineComputation;
+	readonly effect: DefineEffect;
+}
+
+/**
+ * An arbitrary function which is executed with a {@link ReactiveTestScope}.
+ * Since this reactive implementation is presently targeted for test use cases,
+ * it's anticipated that the callback will be either used to perform a test's
+ * logic directly, or to perform some subset of that test logic like setup.
+ *
+ * The function may return a value of any type, which will in turn be returned
+ * from {@link reactiveTestScope} where it is called. As discussed in
+ * {@link ReactiveTestScope}, care should be taken not to break the scope of
+ * reactivity itself.
+ */
+type ReactiveTestScopeCallback<T> = (context: ReactiveTestScope) => T;
+
+/**
+ * Runs an arbitrary {@link ReactiveTestContextCallback} within a
+ * {@link ReactiveTestScope}, and returns the callback's value. See docs for
+ * both for additional detail and considerations.
+ */
+export const reactiveTestScope = <Result>(callback: ReactiveTestScopeCallback<Result>): Result => {
+	interface Dependency {
+		readonly subscribers: Set<Subscriber>;
+	}
+
+	interface Subscriber {
+		readonly execute: VoidFunction;
+		readonly dependencies: Set<Dependency>;
+	}
+
+	const subscribers: Subscriber[] = [];
+
+	/**
+	 * A thunk which produces a (potentially) reactive value when called, and which
+	 * establishes a subscription when called within a reactive context.
+	 */
+	type ReadAtom<T> = Thunk<T>;
+
+	/**
+	 * A function used to update an atomic reactive value, and which updates
+	 * reactive subscriptions when called with an updated value.
+	 *
+	 * @todo "updated value" is not currently checked
+	 * @todo we don't currently accept a callback with the current value, but it
+	 * might be a good idea if we expand usage
+	 */
+	type WriteAtom<T> = (value: T) => T;
+
+	/**
+	 * Provides both read and write access to an atomic reactive value (e.g. a
+	 * "signal"). Note that this is not the interface for mutables.
+	 */
+	interface Atom<T> {
+		readonly read: ReadAtom<T>;
+		readonly write: WriteAtom<T>;
+	}
+
+	/**
+	 * Produces an atomic reactive value (e.g. a "signal").
+	 */
+	type DefineAtom = <T>(initialValue: T) => Atom<T>;
+
+	const cleanupSubscriber = (running: Subscriber) => {
+		const { dependencies } = running;
+
+		for (const dependency of dependencies) {
+			dependency.subscribers.delete(running);
+		}
+
+		dependencies.clear();
+	};
+
+	class AtomicState<T> implements Atom<T>, Dependency {
+		readonly read: ReadAtom<T>;
+		readonly write: WriteAtom<T>;
+		readonly subscribers = new Set<Subscriber>();
+
+		constructor(value: T) {
+			this.read = (): T => {
+				const running = subscribers[subscribers.length - 1];
+
+				if (running) {
+					this.subscribers.add(running);
+
+					running.dependencies.add(this);
+				}
+
+				return value;
+			};
+
+			this.write = (nextValue: T) => {
+				/**
+				 * @see {@link WriteAtom}, this is likely where we'd think about checking
+				 * whether a value change has actually occurred to determine whether
+				 * any subscriptions should be notified.
+				 */
+				value = nextValue;
+
+				for (const subscriber of [...this.subscribers]) {
+					subscriber.execute();
+				}
+
+				return value;
+			};
+		}
+	}
+
+	const atom: DefineAtom = (value) => {
+		return new AtomicState(value);
+	};
+
+	const computed: DefineComputation = <T>(fn: Thunk<T>): Thunk<T> => {
+		const { read, write } = atom<T>(undefined!);
+
+		effect(() => write(fn()));
+
+		return read;
+	};
+
+	const effect: DefineEffect = (effectFn: VoidFunction) => {
+		const execute = () => {
+			cleanupSubscriber(running);
+
+			subscribers.push(running);
+
+			try {
+				effectFn();
+			} finally {
+				subscribers.pop();
+			}
+		};
+
+		const running: Subscriber = {
+			execute,
+			dependencies: new Set(),
+		};
+
+		execute();
+	};
+
+	const mutable = <T extends MutableState>(object: T): T => {
+		// Creating a null-property object ensures we're *only* creating reactive
+		// properties for the explicitly defined members of the input object.
+		const reactiveObject = Object.create(null) as T;
+
+		// Map each "normal" (read/writable) property to a computed get/set property
+		// backed by an Atom. For other properties:
+		//
+		// - Existing computed properties are preserved exactly as they're defined.
+		// - Inherited (e.g. by prototype/class extension) properties are omitted.
+		Object.entries(Object.getOwnPropertyDescriptors(object)).forEach(([key, inputDescriptor]) => {
+			if (inputDescriptor.writable) {
+				const { read, write } = atom(inputDescriptor.value);
+
+				Object.defineProperty(reactiveObject, key, {
+					configurable: false,
+					enumerable: true,
+					get: read,
+					set: write,
+				});
+			} else {
+				Object.defineProperty(reactiveObject, key, inputDescriptor);
+			}
+		});
+
+		return reactiveObject;
+	};
+
+	// Having now established the API available to the reactive scope, we execute
+	// the provided callback with that API. We would likely perform any cleanup
+	// after this call.
+	const result = callback({
+		mutable,
+		computed,
+		effect,
+	});
+
+	return result;
+};