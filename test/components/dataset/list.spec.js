2bd50ee66fce4a073ceb0a20cb0ee68fabdddb1f
Kathleen Tuite <ktuite@getodk.org>
2024-04-02 10:56:54 -0700

    dataset list tests

--- test/components/dataset/list.spec.js
+++ test/components/dataset/list.spec.js
@@ -3,14 +3,29 @@ import { load } from '../../util/http';
 import { mockLogin } from '../../util/session';
 
 describe('DatasetList', () => {
-  beforeEach(mockLogin);
-
   it('sends the correct initial requests', () => {
+    mockLogin();
     testData.extendedDatasets.createPast(1);
     return load('/projects/1/entity-lists', {
       root: false
     }).testRequests([
       { url: '/v1/projects/1/datasets', extended: true }
     ]);
+  });
+
+  describe('new dataset button', () => {
+    it('allows admins to see new button', async () => {
+      mockLogin({ role: 'admin' });
+      testData.extendedDatasets.createPast(1, { name: 'trees' });
+      const app = await load('/projects/1/entity-lists');
+      app.find('#dataset-list-new-button').exists().should.be.true();
+    });
+
+    it('does not allow project viewers to see new button', async () => {
+      mockLogin({ role: 'viewer' });
+      testData.extendedDatasets.createPast(1, { name: 'trees' });
+      const app = await load('/projects/1/entity-lists');
+      app.find('#dataset-list-new-button').exists().should.be.false();
+    });
   });
 });
