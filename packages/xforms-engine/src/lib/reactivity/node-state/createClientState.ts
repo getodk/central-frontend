fa32fe75f6bd1e19f1e538a56de9cedb6a742600
eyelidlessness <gnosis@gmail.com>
2024-04-02 14:26:10 -0700

    Eliminate Solid client state special case…
    … prepare for storing `children` client state as array of node IDs.
    
    For posterity: this special case works fine with the `children` client state solution, but removing it provides an additional opportunity to detect inconsistencies in how the logic is applied with different client reactivity factories.

--- packages/xforms-engine/src/lib/reactivity/node-state/createClientState.ts
+++ packages/xforms-engine/src/lib/reactivity/node-state/createClientState.ts
@@ -1,7 +1,6 @@
 import { getPropertyKeys } from '@odk-web-forms/common/lib/objects/structure.ts';
 import type { ShallowMutable } from '@odk-web-forms/common/types/helpers.js';
 import { createComputed, untrack } from 'solid-js';
-import { createMutable } from 'solid-js/store';
 import type { ReactiveScope } from '../scope.ts';
 import type { EngineState } from './createEngineState.ts';
 import type { SpecifiedState, StateSpec } from './createSpecifiedState.ts';
@@ -32,14 +31,6 @@ export const createClientState = <Spec extends StateSpec>(
 	engineState: EngineState<Spec>,
 	clientStateFactory: SpecifiedClientStateFactory<Spec>
 ): ClientState<Spec> => {
-	// Special case: if we **know** the client is Solid, we also know that the
-	// engine state is already reactive for the client. In which case, we can
-	// skip the client mutable state wrapper and just rely on the fact that the
-	// client-facing `currentState` will still be wrapped in a read-only proxy.
-	if (clientStateFactory === createMutable) {
-		return declareInternalClientRepresentation<ShallowMutable<SpecifiedState<Spec>>>(engineState);
-	}
-
 	const initialState = deriveInitialState(scope, engineState);
 	const clientState = clientStateFactory(initialState);
 
