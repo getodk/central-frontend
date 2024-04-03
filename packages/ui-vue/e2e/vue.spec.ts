72812ee5ae3b061ad53aa71715aef6f3aafd7e99
Sadiq Khoja <sadiqkhoja@gmail.com>
2024-04-03 15:41:43 -0400

    include components files in test project - tsc is complaining

--- packages/ui-vue/e2e/vue.spec.ts
+++ packages/ui-vue/e2e/vue.spec.ts
@@ -1,6 +1,5 @@
-import { expect, test } from '@playwright/test';
+import { test } from '@playwright/test';
 
 test('visits the app root url', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.locator('h1')).toHaveText('ODK Form');
 });
