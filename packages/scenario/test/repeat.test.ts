ab17621c88df9a44f83be01617e744cd9c6e887c
eyelidlessness <gnosis@gmail.com>
2024-04-04 13:50:03 -0700

    This `createRoot` is now superfluous

--- packages/scenario/test/repeat.test.ts
+++ packages/scenario/test/repeat.test.ts
@@ -10,7 +10,6 @@ import {
 	t,
 	title,
 } from '@odk-web-forms/common/test/fixtures/xform-dsl/index.ts';
-import { createRoot } from 'solid-js';
 import { describe, it } from 'vitest';
 import { Scenario } from '../src/Scenario.ts';
 import { CoreMatchers, assertThat, intAnswer, stringAnswer } from '../src/assert.ts';
@@ -20,42 +19,40 @@ describe('Tests ported from JavaRosa - repeats', () => {
 		describe('adding repeat instance', () => {
 			// https://github.com/getodk/javarosa/blob/059321160e6f8dbb3e81d9add61d68dd35b13cc8/src/test/java/org/javarosa/core/model/TriggerableDagTest.java#L785
 			it('updates calculation cascade', async () => {
-				await createRoot(async () => {
-					// prettier-ignore
-					const scenario = await Scenario.init("Add repeat instance", html(
-						head(
-								title("Add repeat instance"),
-								model(
-										mainInstance(t("data id=\"repeat-calcs\"",
-												t("repeat",
-														t("inner1"),
-														t("inner2"),
-														t("inner3")
-												))),
-										bind("/data/repeat/inner2").calculate("2 * ../inner1"),
-										bind("/data/repeat/inner3").calculate("2 * ../inner2"))),
+				// prettier-ignore
+				const scenario = await Scenario.init("Add repeat instance", html(
+					head(
+							title("Add repeat instance"),
+							model(
+									mainInstance(t("data id=\"repeat-calcs\"",
+											t("repeat",
+													t("inner1"),
+													t("inner2"),
+													t("inner3")
+											))),
+									bind("/data/repeat/inner2").calculate("2 * ../inner1"),
+									bind("/data/repeat/inner3").calculate("2 * ../inner2"))),
 
-						body(
-								repeat("/data/repeat",
-										input("/data/repeat/inner1"))
-						)));
+					body(
+							repeat("/data/repeat",
+									input("/data/repeat/inner1"))
+					)));
 
-					scenario.next('/data/repeat[1]');
-					scenario.next('/data/repeat[1]/inner1');
-					scenario.answer(0);
+				scenario.next('/data/repeat[1]');
+				scenario.next('/data/repeat[1]/inner1');
+				scenario.answer(0);
 
-					assertThat(scenario.answerOf('/data/repeat[1]/inner2'), CoreMatchers.is(intAnswer(0)));
-					assertThat(scenario.answerOf('/data/repeat[1]/inner3'), CoreMatchers.is(intAnswer(0)));
+				assertThat(scenario.answerOf('/data/repeat[1]/inner2'), CoreMatchers.is(intAnswer(0)));
+				assertThat(scenario.answerOf('/data/repeat[1]/inner3'), CoreMatchers.is(intAnswer(0)));
 
-					scenario.next('/data/repeat');
-					scenario.createNewRepeat('/data/repeat');
-					scenario.next('/data/repeat[2]/inner1');
+				scenario.next('/data/repeat');
+				scenario.createNewRepeat('/data/repeat');
+				scenario.next('/data/repeat[2]/inner1');
 
-					scenario.answer(1);
+				scenario.answer(1);
 
-					assertThat(scenario.answerOf('/data/repeat[2]/inner2'), CoreMatchers.is(intAnswer(2)));
-					assertThat(scenario.answerOf('/data/repeat[2]/inner3'), CoreMatchers.is(intAnswer(4)));
-				});
+				assertThat(scenario.answerOf('/data/repeat[2]/inner2'), CoreMatchers.is(intAnswer(2)));
+				assertThat(scenario.answerOf('/data/repeat[2]/inner3'), CoreMatchers.is(intAnswer(4)));
 			});
 
 			// Currently fails due to use of absolute XPath expression into repeat
