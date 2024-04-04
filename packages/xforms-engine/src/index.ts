4eae9086bcf5689f7545c7b3c53a907a7d410844
eyelidlessness <gnosis@gmail.com>
2024-04-04 14:04:04 -0700

    Remove xforms-engine exports of `EntryState` APIs…
    … and the vast majority of other types which aren’t explicitly intended to be part of the client interface.
    
    There’s still work to do to refine the client-facing `definition` interfaces.

--- packages/xforms-engine/src/index.ts
+++ packages/xforms-engine/src/index.ts
@@ -1,24 +1,3 @@
-export type { AnyBodyElementDefinition, BodyDefinition } from './body/BodyDefinition.ts';
-export type { ControlDefinition } from './body/control/ControlDefinition.ts';
-export type { InputDefinition } from './body/control/InputDefinition.ts';
-export type {
-	AnySelectDefinition,
-	SelectDefinition,
-} from './body/control/select/SelectDefinition.ts';
-export type { LabelDefinition } from './body/text/LabelDefinition.ts';
-export type { TranslationState } from './state/TranslationState.ts';
-
-export type { AnyDescendantNodeState, DescendantNodeState } from './state/DescendantNodeState.ts';
-export type { AnyChildState, AnyParentState, NodeState } from './state/NodeState.ts';
-export type { RepeatInstanceState } from './state/RepeatInstanceState.ts';
-export type { RepeatSequenceState } from './state/RepeatSequenceState.ts';
-export type { SubtreeState } from './state/SubtreeState.ts';
-export type { ValueNodeState } from './state/ValueNodeState.ts';
-
-export { XFormDefinition } from './XFormDefinition.ts';
-export { EntryState } from './state/EntryState.ts';
-
-// Expose new client interface
 export type * from './client/EngineConfig.ts';
 export type * from './client/FormLanguage.ts';
 export type * from './client/GroupNode.ts';
@@ -41,3 +20,12 @@ export type * from './client/index.ts';
 
 // Expose implementation of new client interface
 export { initializeForm } from './instance/index.ts';
+
+// TODO: notwithstanding potential conflicts with parallel work on `ui-vue`,
+// these are the last remaining references **outside of `xforms-engine`** to
+// anything besides /client/* and the `initializeForm` entrypoint
+// implementation. We'll refine the various `definition` types in due time.
+export type {
+	AnySelectDefinition,
+	SelectDefinition,
+} from './body/control/select/SelectDefinition.ts';
