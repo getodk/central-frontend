73c78cb3ed9f3f17aa01d856e24f06e7c6ac827a
eyelidlessness <gnosis@gmail.com>
2024-04-09 13:23:07 -0700

    Add JSDoc example for `SubtreeNode`

--- packages/xforms-engine/src/client/SubtreeNode.ts
+++ packages/xforms-engine/src/client/SubtreeNode.ts
@@ -21,6 +21,31 @@ export interface SubtreeDefinition extends BaseSubtreeDefinition {
  * `<group>`. A subtree node does not have any direct implications for
  * presentation to users, but its descendants may specify presentational details
  * in their own {@link BaseNode.definition | definition}s.
+ *
+ * @example
+ *
+ * A common `SubtreeNode` case will be a form's `<meta>` element:
+ *
+ * ```xml
+ * <!-- /h:html/h:head/model -->
+ * <instance>
+ *   <data>
+ *     <some-group>
+ *       <some-field />
+ *     </some-group>
+ *     <!-- Note that `meta` does not have a corresponding group body element -->
+ *     <!-- SubtreeNode(/data/meta): { ... -->
+ *     <meta>
+ *       <instanceID/>
+ *     </meta>
+ *     <!-- } -->
+ *   </data>
+ * </instance>
+ * <!-- /h:html/h:body -->
+ * <group ref="/data/some-group">
+ *   <input ref="/data/some-group/some-field" />
+ * </group>
+ * ```
  */
 // TODO: directly test presentation of non-group subtree children/descendants
 export interface SubtreeNode extends BaseNode {
