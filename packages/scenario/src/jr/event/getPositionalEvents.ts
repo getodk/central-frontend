bf8e5b11330ed2dc0faa7351057a93f97fa3768f
eyelidlessness <gnosis@gmail.com>
2024-04-25 16:23:22 -0700

    Fix: don’t include non-relevant nodes as positional “event”s

--- packages/scenario/src/jr/event/getPositionalEvents.ts
+++ packages/scenario/src/jr/event/getPositionalEvents.ts
@@ -1,4 +1,5 @@
 import { UnreachableError } from '@getodk/common/lib/error/UnreachableError.ts';
+import type { AnyNode } from '@getodk/xforms-engine';
 import { isStringInputNode, type RootNode } from '../../client/node-types.ts';
 import { collectFlatNodeList } from '../../client/traversal.ts';
 import { BeginningOfFormEvent } from './BeginningOfFormEvent.ts';
@@ -32,38 +33,47 @@ export type PositionalEvents = readonly [
 	EndOfFormEvent,
 ];
 
+type AnyRelevantNode = AnyNode & { readonly currentState: { readonly relevant: true } };
+
+const isRelevant = (node: AnyNode): node is AnyRelevantNode => {
+	return node.currentState.relevant === true;
+};
+
 export const getPositionalEvents = (instanceRoot: RootNode): PositionalEvents => {
 	const beginning = BeginningOfFormEvent.from(instanceRoot);
 	const nodes = collectFlatNodeList(instanceRoot);
-	const nonTerminalEvents = nodes.flatMap((node): NonTerminalPositionalEvent | readonly [] => {
-		switch (node.nodeType) {
-			case 'root':
-			case 'subtree':
-				return [];
+	const relevantNodes = nodes.filter(isRelevant);
+	const nonTerminalEvents = relevantNodes.flatMap(
+		(node): NonTerminalPositionalEvent | readonly [] => {
+			switch (node.nodeType) {
+				case 'root':
+				case 'subtree':
+					return [];
 
-			case 'repeat-instance':
-				return RepeatInstanceEvent.from(node);
+				case 'repeat-instance':
+					return RepeatInstanceEvent.from(node);
 
-			case 'repeat-range':
-				return PromptNewRepeatEvent.from(node);
+				case 'repeat-range':
+					return PromptNewRepeatEvent.from(node);
 
-			case 'group':
-				return GroupEvent.from(node);
+				case 'group':
+					return GroupEvent.from(node);
 
-			case 'select':
-				return SelectQuestionEvent.from(node);
+				case 'select':
+					return SelectQuestionEvent.from(node);
 
-			case 'string':
-				if (isStringInputNode(node)) {
-					return StringInputQuestionEvent.from(node);
-				}
+				case 'string':
+					if (isStringInputNode(node)) {
+						return StringInputQuestionEvent.from(node);
+					}
 
-				return [];
+					return [];
 
-			default:
-				throw new UnreachableError(node);
+				default:
+					throw new UnreachableError(node);
+			}
 		}
-	});
+	);
 	const end = EndOfFormEvent.from(null);
 
 	return [beginning, ...nonTerminalEvents, end];
