309ddde91212efab5993ebd8bcad428104014040
eyelidlessness <gnosis@gmail.com>
2024-04-28 11:42:15 -0700

    `TriggerableDagTest.java` - restructure grouping of `parsing_forms_with_cycles_*` tests

--- packages/scenario/test/form-definition-validity.test.ts
+++ packages/scenario/test/form-definition-validity.test.ts
@@ -95,150 +95,213 @@ describe('TriggerableDagTest.java', () => {
 			return html(head(title('Some form'), model(...modelChildren)), body(...inputs));
 		};
 
-		describe('parsing forms with cycles by self reference in calculate', () => {
-			/**
-			 * **PORTING NOTES**
-			 *
-			 * We don't currently have explicit cycle detection. We did previously,
-			 * but it was recently removed, as we were able to realize an earlier
-			 * hypothetical goal to push more graph-specific logic into reactivity
-			 * (which itself implements a DAG, albeit more generically).
-			 *
-			 * Porting this test faithfully, by checking the error message it
-			 * produces, would suggest a greater behavioral divergence between
-			 * JavaRosa and web forms than presently exists. As such, this current
-			 * test is marked failing to demonstrate that.
-			 *
-			 * A supplementary test follows, which relaxes the error message check to
-			 * demonstrate that loading the form _does fail as expected_.
-			 *
-			 * Note that while we don't yet intentionally implement error messaging
-			 * for this scenario, it will certainly be a case we want to message
-			 * clearly. It'll be interesting to think about how we might achieve that…
-			 * even if it means reintroducing upfront cycle detection, which would
-			 * ultimately be redundant beyond the error messaging use case.
-			 */
-			it.fails('should fail', async () => {
-				// exceptionRule.expect(XFormParseException.class);
-				// exceptionRule.expectMessage("Cycle detected in form's relevant and calculation logic!");
+		describe('parsing forms with cycles', () => {
+			describe('by self reference in calculate', () => {
+				/**
+				 * **PORTING NOTES**
+				 *
+				 * We don't currently have explicit cycle detection. We did previously,
+				 * but it was recently removed, as we were able to realize an earlier
+				 * hypothetical goal to push more graph-specific logic into reactivity
+				 * (which itself implements a DAG, albeit more generically).
+				 *
+				 * Porting this test faithfully, by checking the error message it
+				 * produces, would suggest a greater behavioral divergence between
+				 * JavaRosa and web forms than presently exists. As such, this current
+				 * test is marked failing to demonstrate that.
+				 *
+				 * A supplementary test follows, which relaxes the error message check
+				 * to demonstrate that loading the form _does fail as expected_.
+				 *
+				 * Note that while we don't yet intentionally implement error messaging
+				 * for this scenario, it will certainly be a case we want to message
+				 * clearly. It'll be interesting to think about how we might achieve
+				 * that… even if it means reintroducing upfront cycle detection, which
+				 * would ultimately be redundant beyond the error messaging use case.
+				 */
+				it.fails('should fail', async () => {
+					// exceptionRule.expect(XFormParseException.class);
+					// exceptionRule.expectMessage("Cycle detected in form's relevant and calculation logic!");
 
-				const init = async () => {
-					return Scenario.init(
-						'Some form',
-						buildFormForDagCyclesCheck(bind('/data/count').type('int').calculate('. + 1'))
+					const init = async () => {
+						return Scenario.init(
+							'Some form',
+							buildFormForDagCyclesCheck(bind('/data/count').type('int').calculate('. + 1'))
+						);
+					};
+
+					await expect(init).rejects.toThrow(
+						"Cycle detected in form's relevant and calculation logic!"
 					);
-				};
+				});
 
-				await expect(init).rejects.toThrow(
-					"Cycle detected in form's relevant and calculation logic!"
-				);
+				/**
+				 * **PORTING NOTES**
+				 *
+				 * This test is supplementary to the one more faithfully ported from
+				 * JavaRosa above (and that test's porting notes explain the reasoning
+				 * for this in detail).
+				 *
+				 * Specific details of this test:
+				 *
+				 * 1. It seemed redundant to preserve (commented out) JavaRosa's
+				 *    different approach to error condition assertions. But if we decide
+				 *    that those commented out lines may be valuable for future
+				 *    maintenance (i.e. as an aid to synchronization with JavaRosa), and
+				 *    if we also decide that this supplemental test is suitable to stand
+				 *    in for the more faithful port, we may want to move those lines
+				 *    into this test before removing the other.
+				 *
+				 * 2. A first pass had included a check for the actual error message
+				 *    that's currently produced. The thinking was that, since the error
+				 *    is currently expected but doesn't express the specific intent, we
+				 *    could at least have some additional intent **here** by making the
+				 *    test a bit more brittle (as a reminder that the user-facing intent
+				 *    is unaddressed). Unfortunately, each of our supported test
+				 *    environments currently produces a different error message. So for
+				 *    now, we just check that an error is produced at all.
+				 */
+				it('should fail (supplementary/alternate test with bogus error message check)', async () => {
+					const init = async () => {
+						return Scenario.init(
+							'Some form',
+							buildFormForDagCyclesCheck(bind('/data/count').type('int').calculate('. + 1'))
+						);
+					};
+
+					await expect(init).rejects.toThrow();
+				});
 			});
 
 			/**
 			 * **PORTING NOTES**
 			 *
-			 * This test is supplementary to the one more faithfully ported from
-			 * JavaRosa above (and that test's porting notes explain the reasoning for
-			 * this in detail).
-			 *
-			 * Specific details of this test:
-			 *
-			 * 1. It seemed redundant to preserve (commented out) JavaRosa's different
-			 *    approach to error condition assertions. But if we decide that those
-			 *    commented out lines may be valuable for future maintenance (i.e. as
-			 *    an aid to synchronization with JavaRosa), and if we also decide that
-			 *    this supplemental test is suitable to stand in for the more faithful
-			 *    port, we may want to move those lines into this test before removing
-			 *    the other.
-			 *
-			 * 2. A first pass had included a check for the actual error message
-			 *    that's currently produced. The thinking was that, since the error is
-			 *    currently expected but doesn't express the specific intent, we could
-			 *    at least have some additional intent **here** by making the test a
-			 *    bit more brittle (as a reminder that the user-facing intent is
-			 *    unaddressed). Unfortunately, each of our supported test environments
-			 *    currently produces a different error message. So for now, we just
-			 *    check that an error is produced at all.
+			 * With the above tests demonstrating two approaches to porting JavaRosa's
+			 * cycle detection tests, each remaining cycle detection test will
+			 * collapse both approaches into a single ported test, with JavaRosa's
+			 * error checks commented out, and a relaxed error check actually
+			 * executed. (Some variance in the approach to error checking may be
+			 * necessary, as is the case for the first test in this suite. A note will
+			 * be included whenever such variation becomes necessary.)
 			 */
-			it('should fail (supplementary/alternate test with bogus error message check)', async () => {
-				const init = async () => {
-					return Scenario.init(
-						'Some form',
-						buildFormForDagCyclesCheck(bind('/data/count').type('int').calculate('. + 1'))
-					);
-				};
+			describe('in calculate', () => {
+				/**
+				 * **PORTING NOTES**
+				 *
+				 * 0. This test uses a less idiomatic error condition assertion. For
+				 *    some reason, rather than failing, Vitest hangs indefinitely on a
+				 *    `rejects.toThrow` assertion (it never even times out; but I
+				 *    suspect we'd need a cancelable `Promise` implementation to be able
+				 *    to work around that).
+				 *
+				 * 1. This is the first big surprise to come up in the porting effort! I
+				 *    would very much have expected Solid's reactivity to produce some
+				 *    kind of error for this cycle. Since it doesn't, it seems we will
+				 *    definitely need upfront cycle detection logic.
+				 *
+				 * 2. It's not immediately clear if this surprise indicates any (or some
+				 *    combination) of:
+				 *
+				 *    - an actual bug (logic error) in the way web forms currently sets
+				 *      up the form's computations
+				 *
+				 *    - some aspect of Solid's internal graph logic which allows for
+				 *      cycles to short circuit under some circumstances
+				 *
+				 * 3. What the actual form definition produces is `NaN` for each field.
+				 *    It also produces that even if each field has a default value. I'm
+				 *    super curious to investigate this further, as I would have
+				 *    expected, if there is some form of short circuiting happening, for
+				 *    one or more default values to be preserved (and even perhaps for
+				 *    one or more of the calculations to succeed).
+				 *
+				 * 4. In hindsight, thinking about the implications of that last point,
+				 *    **of course** we still want explicit cycle detection: an
+				 *    inherently invalid form definition should fail fast, without ever
+				 *    being able to reach some invalid but partially complete loading
+				 *    state.
+				 */
+				it.fails('should fail', async () => {
+					// exceptionRule.expect(XFormParseException.class);
+					// exceptionRule.expectMessage("Cycle detected in form's relevant and calculation logic!");
 
-				await expect(init).rejects.toThrow();
+					let caught: unknown = null;
+
+					try {
+						await Scenario.init(
+							'Some form',
+							buildFormForDagCyclesCheck(
+								bind('/data/a').type('int').calculate('/data/b + 1'),
+								bind('/data/b').type('int').calculate('/data/c + 1'),
+								bind('/data/c').type('int').calculate('/data/a + 1')
+							)
+						);
+					} catch (error) {
+						caught = error;
+					}
+
+					expect(caught).toBeInstanceOf(Error);
+				});
 			});
-		});
 
-		/**
-		 * **PORTING NOTES**
-		 *
-		 * With the above tests demonstrating two approaches to porting JavaRosa's
-		 * cycle detection tests, each remaining cycle detection test will collapse
-		 * both approaches into a single ported test, with JavaRosa's error checks
-		 * commented out, and a relaxed error check actually executed. (Some
-		 * variance in the approach to error checking may be necessary, as is the
-		 * case for the first test in this suite. A note will be included whenever
-		 * such variation becomes necessary.)
-		 */
-		describe('parsing forms with cycles in calculate', () => {
 			/**
 			 * **PORTING NOTES**
 			 *
-			 * 0. This test uses a less idiomatic error condition assertion. For some
-			 *    reason, rather than failing, Vitest hangs indefinitely on a
-			 *    `rejects.toThrow` assertion (it never even times out; but I suspect
-			 *    we'd need a cancelable `Promise` implementation to be able to work
-			 *    around that).
-			 *
-			 * 1. This is the first big surprise to come up in the porting effort! I
-			 *    would very much have expected Solid's reactivity to produce some
-			 *    kind of error for this cycle. Since it doesn't, it seems we will
-			 *    definitely need upfront cycle detection logic.
-			 *
-			 * 2. It's not immediately clear if this surprise indicates any (or some
-			 *    combination) of:
-			 *
-			 *    - an actual bug (logic error) in the way web forms currently sets up
-			 *      the form's computations
-			 *
-			 *    - some aspect of Solid's internal graph logic which allows for
-			 *      cycles to short circuit under some circumstances
-			 *
-			 * 3. What the actual form definition produces is `NaN` for each field. It
-			 *    also produces that even if each field has a default value. I'm super
-			 *    curious to investigate this further, as I would have expected, if
-			 *    there is some form of short circuiting happening, for one or more
-			 *    default values to be preserved (and even perhaps for one or more of
-			 *    the calculations to succeed).
-			 *
-			 * 4. In hindsight, thinking about the implications of that last point,
-			 *    **of course** we still want explicit cycle detection: an inherently
-			 *    invalid form definition should fail fast, without ever being able to
-			 *    reach some invalid but partially complete loading state.
+			 * It appears there are several tests of a similar shape, for each
+			 * applicable expression. We may want to condense them into a table test?
 			 */
-			it.fails('should fail', async () => {
-				// exceptionRule.expect(XFormParseException.class);
-				// exceptionRule.expectMessage("Cycle detected in form's relevant and calculation logic!");
+			describe('by self reference in relevance', () => {
+				it('should fail', async () => {
+					// exceptionRule.expect(XFormParseException.class);
+					// exceptionRule.expectMessage("Cycle detected in form's relevant and calculation logic!");
 
-				let caught: unknown = null;
+					const init = async () => {
+						return Scenario.init(
+							'Some form',
+							buildFormForDagCyclesCheck(bind('/data/count').type('int').relevant('. > 0'))
+						);
+					};
 
-				try {
-					await Scenario.init(
-						'Some form',
-						buildFormForDagCyclesCheck(
-							bind('/data/a').type('int').calculate('/data/b + 1'),
-							bind('/data/b').type('int').calculate('/data/c + 1'),
-							bind('/data/c').type('int').calculate('/data/a + 1')
-						)
-					);
-				} catch (error) {
-					caught = error;
-				}
+					await expect(init).rejects.toThrow();
+				});
+			});
 
-				expect(caught).toBeInstanceOf(Error);
+			/**
+			 * **PORTING NOTES**
+			 *
+			 * Rephrase? This is a reference to the `readonly` expression, per spec.
+			 * Is there a colloquial reason to prefer referring to it as two words?
+			 */
+			describe('by self reference in [readonly] read only condition', () => {
+				it('should fail', async () => {
+					// exceptionRule.expect(XFormParseException.class);
+					// exceptionRule.expectMessage("Cycle detected in form's relevant and calculation logic!");
+
+					const init = async () => {
+						return Scenario.init(
+							'Some form',
+							buildFormForDagCyclesCheck(bind('/data/count').type('int').readonly('. > 10'))
+						);
+					};
+
+					await expect(init).rejects.toThrow();
+				});
+			});
+
+			describe('by self reference in required condition', () => {
+				it('should fail', async () => {
+					// 	exceptionRule.expect(XFormParseException.class);
+					// exceptionRule.expectMessage("Cycle detected in form's relevant and calculation logic!");
+
+					const init = async () => {
+						return Scenario.init(
+							'Some form',
+							buildFormForDagCyclesCheck(bind('/data/count').type('int').required('. > 10'))
+						);
+					};
+
+					await expect(init).rejects.toThrow();
+				});
 			});
 		});
 	});
