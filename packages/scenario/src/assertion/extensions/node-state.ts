dfab1bafdc54156a4ae19c4e4fe3302aefbab621
eyelidlessness <gnosis@gmail.com>
2024-04-29 15:21:36 -0700

    Custom node state assertions for `readonly`…
    … note recommendation to use negation of `toBeReadonly` assertion rather than the less clearly named `toBeEnabled` custom assertion also introduced in this commit.

--- packages/scenario/src/assertion/extensions/node-state.ts
+++ packages/scenario/src/assertion/extensions/node-state.ts
@@ -70,6 +70,38 @@ const nodeStateExtensions = extendExpect(expect, {
 	toBeNonRelevant: new StaticConditionExpectExtension(assertEngineNode, {
 		currentState: { relevant: false },
 	}),
+
+	/**
+	 * **PORTING NOTES**
+	 *
+	 * This assertion extension's name is currently derived from JavaRosa's
+	 * assertions phrased like `assertThat(..., is(enabled()))`. It is clearly
+	 * concerned with a node **not** being read-only (i.e. either its `bind` has
+	 * no `readonly` expression, or that expression evaluates to `false`).
+	 *
+	 * The corresponding affirmative assertion is (more obviously) named
+	 * `readOnly`.
+	 *
+	 * These assertion semantics are preserved for now, but we should consider
+	 * using negated assertions of the more clearly named `toBeReadonly` custom
+	 * assertion below (e.g. `expect(...).not.toBeReadonly()`).
+	 */
+	toBeEnabled: new StaticConditionExpectExtension(assertEngineNode, {
+		currentState: { readonly: false },
+	}),
+
+	/**
+	 * **PORTING NOTES**
+	 *
+	 * While JavaRosa uses the more colloquially comfortable `readOnly`
+	 * capitalization (which would correspond to the term being two words or
+	 * hyphenated), it felt more appropriate to preserve the single-word nature
+	 * here since it directly references XForms spec semantics where the state is
+	 * determined by the result of a `<bind readonly>` expression.
+	 */
+	toBeReadonly: new StaticConditionExpectExtension(assertEngineNode, {
+		currentState: { readonly: true },
+	}),
 });
 
 type NodeStateExtensions = typeof nodeStateExtensions;
