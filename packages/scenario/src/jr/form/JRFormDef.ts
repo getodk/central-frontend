72d49199a53745c66c516890c4806d3821e47546
eyelidlessness <gnosis@gmail.com>
2024-05-02 13:37:51 -0700

    RecordAudioActionTest.java - port second `odk:recordaudio` test
    Separate commit mostly to show why the ancillary interface/classes are added.

--- packages/scenario/src/jr/form/JRFormDef.ts
+++ packages/scenario/src/jr/form/JRFormDef.ts
@@ -1,4 +1,5 @@
 import type { RootNode } from '@getodk/xforms-engine';
+import { UnclearApplicabilityError } from '../../error/UnclearApplicabilityError.ts';
 
 /**
  * @todo Hopefully we can keep this interface extremely minimal. It currently
@@ -9,5 +10,19 @@ export class JRFormDef {
 
 	getMainInstance(): RootNode {
 		return this.instanceRoot;
+	}
+
+	/**
+	 * Note: this appears to look up actions by their local tag name. For
+	 * instance, the first such call in a ported test looks up `recordaudio`,
+	 * which evidently corresponds to `odk:recordaudio` (which is also not
+	 * mentioned in the
+	 * {@link https://getodk.github.io/xforms-spec/#events | ODK XForms specification's Events section}).
+	 *
+	 * Also note: this method is hard-coded to throw. We should consider other
+	 * approaches to assertions which currently call it.
+	 */
+	hasAction(_actionName: string): boolean {
+		throw new UnclearApplicabilityError("JavaRosa's `FormDef.hasAction`");
 	}
 }
