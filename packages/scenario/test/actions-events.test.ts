eb6ee812529a2e2b1d04abaeb9f97f3f8701c5c0
eyelidlessness <gnosis@gmail.com>
2024-05-02 10:22:16 -0700

    Port MultipleEventsTest.java

--- packages/scenario/test/actions-events.test.ts
+++ packages/scenario/test/actions-events.test.ts
@@ -211,4 +211,168 @@ describe('Actions/Events', () => {
 			});
 		});
 	});
+
+	describe('MultipleEventsTest.java', () => {
+		/**
+		 * **PORTING NOTES**
+		 *
+		 * `getDisplayText` -> `getValue`, as there doesn't appear to be any
+		 * difference in expected semantics. Original usage is commented above each
+		 * converted case.
+		 */
+		describe('nested [odk-instance-first-load] first load event', () => {
+			it.fails('sets [the] value', async () => {
+				const scenario = await Scenario.init('multiple-events.xml');
+
+				// assertThat(scenario.answerOf("/data/nested-first-load").getDisplayText(), is("cheese"));
+				expect(scenario.answerOf('/data/nested-first-load').getValue()).toBe('cheese');
+			});
+
+			describe('in group', () => {
+				it.fails('sets [the] value', async () => {
+					const scenario = await Scenario.init('multiple-events.xml');
+
+					// assertThat(scenario.answerOf("/data/my-group/nested-first-load-in-group").getDisplayText(), is("more cheese"));
+					expect(scenario.answerOf('/data/my-group/nested-first-load-in-group').getValue()).toBe(
+						'more cheese'
+					);
+				});
+			});
+		});
+
+		describe('serialized and deserialized nested [odk-instance-first-load] first load event', () => {
+			/**
+			 * **PORTING NOTES**
+			 *
+			 * - Fails on all of serde, new instance, assertion of the expected value
+			 *   from the `setvalue` action/`odk-instance-first-load` event
+			 *
+			 * - In general, if we determine a test is pertinent with
+			 *   `serializeAndDeserializeForm` followed by `newInstance`, it probably
+			 *   makes sense for `newInstance` to actually return a new instance
+			 *   (presumably itself producing a new **instance of `Scenario`**). We
+			 *   should consider a followup introducing that `Scenario` API change,
+			 *   with tests updated to reference the instance it produces rather than
+			 *   mutating the deserialized `Scenario`.
+			 *
+			 * - We should also do a full pass to ensure that pattern holds. If there
+			 *   are other cases of `newInstance` which don't first
+			 *   `serializeAndDeserialize`, we'll want to ensure they similarly
+			 *   reference a newly produced instance.
+			 *
+			 * - While we're at it, let's consider striking that `And` from the serde
+			 *   method, and have both `serialize`/`deserialize` methods (the former
+			 *   producing a serialized value, the latter probably a static method
+			 *   accepting that serialized value).
+			 */
+			it.fails('sets [the] value', async () => {
+				const scenario = await Scenario.init('multiple-events.xml');
+
+				const deserializedScenario = await scenario.serializeAndDeserializeForm();
+
+				await deserializedScenario.newInstance();
+
+				// assertThat(deserializedScenario.answerOf("/data/nested-first-load").getDisplayText(), is("cheese"));
+				expect(deserializedScenario.answerOf('/data/nested-first-load').getValue()).toBe('cheese');
+			});
+
+			describe('in group', () => {
+				it.fails('sets [the] value', async () => {
+					const scenario = await Scenario.init('multiple-events.xml');
+
+					const deserializedScenario = await scenario.serializeAndDeserializeForm();
+
+					await deserializedScenario.newInstance();
+
+					// assertThat(deserializedScenario.answerOf("/data/my-group/nested-first-load-in-group").getDisplayText(), is("more cheese"));
+					expect(
+						deserializedScenario.answerOf('/data/my-group/nested-first-load-in-group').getValue()
+					).toBe('more cheese');
+				});
+			});
+		});
+
+		describe('nested [odk-instance-first-load] first load and [xforms-value-changed] value changed events', () => {
+			/**
+			 * **PORTING NOTES**
+			 *
+			 * It may also make more sense to rephrase all of the permutations of
+			 * `setsValue` to both reference `setvalue` (per spec) **and** provide a
+			 * consistent BDD-ish `it [...]` test description format.
+			 */
+			it.fails('set[s the] value', async () => {
+				const scenario = await Scenario.init('multiple-events.xml');
+
+				// assertThat(scenario.answerOf("/data/my-calculated-value").getDisplayText(), is("10"));
+				expect(scenario.answerOf('/data/my-calculated-value').getValue()).toBe('10');
+
+				scenario.answer('/data/my-value', '15');
+
+				// assertThat(scenario.answerOf("/data/my-calculated-value").getDisplayText(), is("30"));
+				expect(scenario.answerOf('/data/my-calculated-value').getValue()).toBe('30');
+			});
+		});
+
+		describe('serialized and deserialized nested [odk-instance-first-load] first load and [xforms-value-changed] value changed events', () => {
+			/**
+			 * **PORTING NOTES**
+			 *
+			 * Also fails on all of serde, new instance, ported assertions.
+			 */
+			it.fails('set[s the] value', async () => {
+				const scenario = await Scenario.init('multiple-events.xml');
+
+				const deserializedScenario = await scenario.serializeAndDeserializeForm();
+
+				await deserializedScenario.newInstance();
+
+				// assertThat(deserializedScenario.answerOf("/data/my-calculated-value").getDisplayText(), is("10"));
+				expect(deserializedScenario.answerOf('/data/my-calculated-value').getValue()).toBe('10');
+
+				deserializedScenario.answer('/data/my-value', '15');
+
+				// assertThat(deserializedScenario.answerOf("/data/my-calculated-value").getDisplayText(), is("30"));
+				expect(deserializedScenario.answerOf('/data/my-calculated-value').getValue()).toBe('30');
+			});
+		});
+
+		describe('invalid event names', () => {
+			/**
+			 * **PORTING NOTES**
+			 *
+			 * - We have discussed, but not yet actually implemented, producing Result
+			 *   types rather than throwing, throughout the engine/client interface.
+			 *   We should consider a more general description of this test that
+			 *   doesn't presuppose the mechanism of error propagation.
+			 *
+			 * - The ported test will, **for now**, be adapted to the equivalent
+			 *   assertions for checking a thrown error (well, in our case a rejected
+			 *   `Promise`). This shouldn't detract from the above point, it's just
+			 *   the most reasonable way to preserve the current intent of the test as
+			 *   ported from JavaRosa.
+			 *
+			 * - As with other ported tests checking for thrown errors/rejected
+			 *   `Promise`s, the original assertion code is commented out and an
+			 *   equivalent follows. The error message text is also checked, as it
+			 *   seems likely this general category of error messaging would be good
+			 *   to align on.
+			 *
+			 * - Test currently fails: beyond current lack of support for
+			 *   actions/events generally, we also don't yet produce any errors and/or
+			 *   warnings on unsupported features.
+			 */
+			it.fails('throw[s an] exception', async () => {
+				// expectedException.expect(XFormParseException.class);
+				// expectedException.expectMessage("An action was registered for unsupported events: odk-inftance-first-load, my-fake-event");
+
+				const init = async () => {
+					return Scenario.init('invalid-events.xml');
+				};
+
+				await expect(init).rejects.toThrowError(
+					'An action was registered for unsupported events: odk-inftance-first-load, my-fake-event'
+				);
+			});
+		});
+	});
 });
