0a19a1459ac1c7e4b0be17c9b42f99d301d45b23
eyelidlessness <gnosis@gmail.com>
2024-05-28 10:22:58 -0700

    Update SelectChoice.getChild porting notes per PR comment

--- packages/scenario/src/jr/select/SelectChoice.ts
+++ packages/scenario/src/jr/select/SelectChoice.ts
@@ -17,14 +17,23 @@ export class SelectChoice extends ComparableChoice {
 	}
 
 	/**
-	 * @todo This appears to have been introduced in JavaRosa to support GeoJSON
-	 * functionality. The
-	 * {@link https://github.com/getodk/javarosa/pull/660 | pull request}
-	 * describes the intent to support accessing a well-known `geometry` child by
-	 * name. There are other tests, however, referencing arbitrary children by
-	 * name as well. This is currently unimplemented (and will cause failures in
-	 * tests accessing it, even if other functionality is introduced to unblock
-	 * code paths leading to its access) until we deem it appropriate to address.
+	 * **PORTING NOTES**
+	 *
+	 * This was
+	 * {@link https://github.com/getodk/javarosa/pull/660 | introduced in JavaRosa to support geo functionality}.
+	 * The method may access a well-known `geometry` child (or any arbitrary
+	 * child) by name,
+	 * {@link https://github.com/getodk/web-forms/pull/110#discussion_r1610611805 | where}…
+	 *
+	 * > Could be a repeat, a secondary instance or an external secondary instance
+	 * > (csv, xml or geojson). The `map` appearance that uses the `geometry`
+	 * > child for mapping works identically for all of those.
+	 *
+	 * @todo This is currently unimplemented (and will cause failures in tests
+	 * accessing it, even if other functionality is introduced to unblock code
+	 * paths leading to its access) until we deem it appropriate to address.
+	 * Addressing it will likely involve understanding how it relates to and/or
+	 * affected tests can be expressed at an integration level.
 	 */
 	getChild(_childName: string): string | null {
 		throw new UnclearApplicabilityError(
