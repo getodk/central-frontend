43f3e10f2fb7bbad43c1cb7bf26d2b61c4f17757
Sadiq Khoja <sadiqkhoja@gmail.com>
2024-06-28 18:41:51 -0400

    fix: include css in the built js file

--- packages/web-forms/src/index.ts
+++ packages/web-forms/src/index.ts
@@ -1,17 +1,10 @@
 import { webFormsPlugin } from './WebFormsPlugin';
 import OdkWebForm from './components/OdkWebForm.vue';
 
-import icomoon from './assets/css/icomoon.css?inline';
-import theme from './themes/2024-light/theme.scss?inline';
+import './assets/css/icomoon.css';
+import './themes/2024-light/theme.scss';
 
 // TODO/sk: Purge it - using postcss-purgecss
-import primeflex from 'primeflex/primeflex.css?inline';
-
-const styles = [icomoon, theme, primeflex].join('\n\n');
-const stylesheet = new CSSStyleSheet();
-
-stylesheet.replaceSync(styles);
-
-document.adoptedStyleSheets.push(stylesheet);
+import 'primeflex/primeflex.css';
 
 export { OdkWebForm, webFormsPlugin };
