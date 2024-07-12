2d491e234b9cd671c69ec3214da8826fe91afca3
eyelidlessness <eyelidlessness@users.noreply.github.com>
2024-07-12 11:41:44 -0700

    Mitigate mysterious Vitest + Webkit + Vue environment flakiness (#165)
    * Fix: dedupe Vite dependency in yarn.lock
    
    It’s unclear why both versions made it through the last round of updates. This is *possibly* related to some of the flakiness we’re seeing, but it’s safe either way.
    
    Presumably `postcss` is also deduped as a transitive dep somewhere? Shrug emoji!
    
    * Mitigation: web-forms/vue/vitest/webkit flakiness (1 of 2)
    
    Adds a bit of delay in global setup if the test run is in webkit, and if there’s not already a `.vite/deps` cache (as a heuristic to differentiate first/subsequent test runs).
    
    * Mitigation: web-forms/vue/vitest/webkit flakiness (2 of 2)
    
    For `web-forms` only (as it seems to consistently be the package showing flakiness), and for `webkit` runs only (same reasoning), attempt test run a second time if first run fails.

--- packages/web-forms/vitest.config.ts
+++ packages/web-forms/vitest.config.ts
@@ -1,5 +1,6 @@
 import type { CollectionValues } from '@getodk/common/types/collections/CollectionValues.ts';
 import type { VitestTestConfig } from '@getodk/common/types/vitest-config.ts';
+import { existsSync } from 'node:fs';
 import { fileURLToPath } from 'node:url';
 import { configDefaults, defineConfig, mergeConfig } from 'vitest/config';
 import viteConfig from './vite.config';
@@ -29,6 +30,22 @@ const BROWSER_ENABLED = BROWSER_NAME != null;
 
 const TEST_ENVIRONMENT = BROWSER_ENABLED ? 'node' : 'jsdom';
 
+const globalSetup: string[] = [];
+
+/**
+ * @todo this is (hopefully!) temporary. Adds a delay when testing in
+ * `webkit`, to help mitigate flakiness that seems to be rooted in
+ * first-run timing issues (where "first" = "no Vite cache"; the issue was
+ * much more consistently reproducible in a state where
+ * `node_modules/.vite` is not present).
+ */
+const webkitFlakinessMitigations =
+	BROWSER_NAME === 'webkit' && !existsSync('./node_modules/.vite/deps');
+
+if (webkitFlakinessMitigations) {
+	globalSetup.push('./tests/globalSetup/mitigate-webkit-flakiness.ts');
+}
+
 export default mergeConfig(
 	viteConfig,
 	defineConfig({
@@ -37,12 +54,17 @@ export default mergeConfig(
 				enabled: BROWSER_ENABLED,
 				name: BROWSER_NAME!,
 				provider: 'playwright',
+				fileParallelism: false,
 				headless: true,
 				screenshotFailures: false,
 			},
 			environment: TEST_ENVIRONMENT,
 			exclude: [...configDefaults.exclude, 'e2e/**'],
 			root: fileURLToPath(new URL('./', import.meta.url)),
+
+			/** @see {@link webkitFlakinessMitigations} */
+			globalSetup,
+
 			// Suppress the console error log about parsing CSS stylesheet
 			// This is an open issue of jsdom
 			// see primefaces/primevue#4512 and jsdom/jsdom#2177
