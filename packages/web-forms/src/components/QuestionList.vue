b8d8b26ffe1ad088c303a08734c02dfdb5ef648a
eyelidlessness <gnosis@gmail.com>
2024-09-13 13:17:15 -0700

    web-forms: incorporate support for stub range, rank, trigger, upload nodes…
    … and ensure that those nodes are displayed to users as unsupported.
    
    I have manually validated this with a `trigger` node (via the same form which prompted this work).

--- packages/web-forms/src/components/QuestionList.vue
+++ packages/web-forms/src/components/QuestionList.vue
@@ -2,18 +2,15 @@
 import type {
 	GeneralChildNode,
 	GroupNode,
-	AnyControlNode as QuestionNode,
+	ModelValueNode,
 	RepeatRangeNode,
+	SubtreeNode,
 } from '@getodk/xforms-engine';
 import FormGroup from './FormGroup.vue';
 import FormQuestion from './FormQuestion.vue';
 import RepeatRange from './RepeatRange.vue';
 
 defineProps<{ nodes: readonly GeneralChildNode[] }>();
-
-const isQuestionNode = (node: GeneralChildNode): node is QuestionNode => {
-	return node.nodeType === 'string' || node.nodeType === 'select' || node.nodeType == 'note';
-};
 
 const isGroupNode = (node: GeneralChildNode): node is GroupNode => {
 	return node.nodeType === 'group';
@@ -24,19 +21,24 @@ const isRepeatRangeNode = (node: GeneralChildNode): node is RepeatRangeNode => {
 		node.nodeType === 'repeat-range:controlled' || node.nodeType === 'repeat-range:uncontrolled'
 	);
 };
+
+const isModelOnlyNode = (node: GeneralChildNode): node is ModelValueNode | SubtreeNode => {
+	return node.nodeType === 'model-value' || node.nodeType === 'subtree';
+};
 </script>
 
 <template>
 	<template v-for="node in nodes" :key="node.nodeId">
 		<template v-if="node.currentState.relevant">
-			<!-- Render leaf nodes like string, select, etc -->
-			<FormQuestion v-if="isQuestionNode(node)" :question="node" />
-
 			<!-- Render group nodes -->
 			<FormGroup v-if="isGroupNode(node)" :node="node" />
 
 			<!-- Render repeat nodes -->
-			<RepeatRange v-if="isRepeatRangeNode(node)" :node="node" />
+			<RepeatRange v-else-if="isRepeatRangeNode(node)" :node="node" />
+
+
+			<!-- Render leaf nodes like string, select, etc -->
+			<FormQuestion v-else-if="!isModelOnlyNode(node)" :question="node" />
 		</template>
 	</template>
 </template>
