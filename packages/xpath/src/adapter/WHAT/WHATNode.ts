2e8d11395209e082f94d3b6d0744cd7d3a9089ef
eyelidlessness <gnosis@gmail.com>
2024-10-27 09:33:43 -0700

    TEMPORARY: `XPath*` node interfaces extend their `WHAT*` counterparts
    This will allow us to migrate portions of WHATWG DOM access incrementally, across multiple commits.
    
    **NOTE:** This commit is isolated this way in hopes of a straightforward revert when it no longer serves its purpose.

--- packages/xpath/src/adapter/WHAT/WHATNode.ts
+++ packages/xpath/src/adapter/WHAT/WHATNode.ts
@@ -1,45 +1,37 @@
 import type { XPathCustomUnwrappableNode } from '../interface/XPathCustomUnwrappableNode.ts';
-import type {
-	XPathAttribute,
-	XPathComment,
-	XPathDocument,
-	XPathElement,
-	XPathNamespaceDeclaration,
-	XPathProcessingInstruction,
-	XPathText,
-} from '../interface/XPathNode.ts';
+import type { XPathNodeKindKey } from '../interface/XPathNode.ts';
 
-export interface WHATDocument
-	extends Document,
-		XPathDocument,
-		XPathCustomUnwrappableNode<Document> {}
+export interface WHATDocument extends Document, XPathCustomUnwrappableNode<Document> {
+	readonly [XPathNodeKindKey]: 'document';
+}
 
-export interface WHATElement extends Element, XPathElement, XPathCustomUnwrappableNode<Element> {}
+export interface WHATElement extends Element, XPathCustomUnwrappableNode<Element> {
+	readonly [XPathNodeKindKey]: 'element';
+}
 
-export interface WHATNamespaceDeclaration
-	extends Attr,
-		XPathNamespaceDeclaration,
-		XPathCustomUnwrappableNode<Attr> {}
+export interface WHATNamespaceDeclaration extends Attr, XPathCustomUnwrappableNode<Attr> {
+	readonly [XPathNodeKindKey]: 'namespace_declaration';
+}
 
-export interface WHATAttribute extends Attr, XPathAttribute, XPathCustomUnwrappableNode<Attr> {}
+export interface WHATAttribute extends Attr, XPathCustomUnwrappableNode<Attr> {
+	readonly [XPathNodeKindKey]: 'attribute';
+}
 
-interface WHATCDATASectionNode
+export interface WHATText
 	extends CDATASection,
-		XPathText,
-		XPathCustomUnwrappableNode<CDATASection> {}
+		Text,
+		XPathCustomUnwrappableNode<CDATASection | Text> {
+	readonly [XPathNodeKindKey]: 'text';
 
-interface WHATTextNode extends Text, XPathText, XPathCustomUnwrappableNode<Text> {}
+	readonly nodeType: Node['CDATA_SECTION_NODE'] | Node['TEXT_NODE'];
+}
+export interface WHATComment extends Comment, XPathCustomUnwrappableNode<Comment> {
+	readonly [XPathNodeKindKey]: 'comment';
+}
 
-// prettier-ignore
-export type WHATText =
-	| WHATCDATASectionNode
-	| WHATTextNode;
-
-export interface WHATComment extends Comment, XPathComment, XPathCustomUnwrappableNode<Comment> {}
-
-export interface WHATProcessingInstruction
-	extends ProcessingInstruction,
-		XPathProcessingInstruction {}
+export interface WHATProcessingInstruction extends ProcessingInstruction {
+	readonly [XPathNodeKindKey]: 'processing_instruction';
+}
 
 type UnwrappableWHATNodeUnion<T, U> = T & XPathCustomUnwrappableNode<U>;
 
@@ -83,3 +75,26 @@ export type WHATNamedNode = UnwrappableWHATNodeUnion<
 	// eslint-disable-next-line @typescript-eslint/sort-type-constituents
 	Element | Attr
 >;
+
+/**
+ * @todo This is temporary! It can be removed once existing WHAT Working Group
+ * DOM access is fully isolated to the WHAT adapter. In so doing, it also makes
+ * sense to invert the interface extension relationship and remove direct
+ * {@link XPathNodeKindKey} definitions on each of the `WHAT*` interfaces. (We
+ * cannot do that now, because it would create an inheritance cycle.)
+ */
+declare module '../interface/XPathNode.ts' {
+	export interface XPathDocument extends WHATDocument {}
+
+	export interface XPathElement extends WHATElement {}
+
+	export interface XPathNamespaceDeclaration extends WHATNamespaceDeclaration {}
+
+	export interface XPathAttribute extends WHATAttribute {}
+
+	export interface XPathText extends WHATText {}
+
+	export interface XPathComment extends WHATComment {}
+
+	export interface XPathProcessingInstruction extends WHATProcessingInstruction {}
+}
