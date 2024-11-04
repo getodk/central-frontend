b4514f4293c9ce134a0957c0bd9a4a86def5c830
eyelidlessness <gnosis@gmail.com>
2024-11-04 12:07:26 -0800

    Engine adapter: implement XPathNodeKindAdapter sub-interface

--- packages/xforms-engine/src/integration/xpath/adapter/engineDOMAdapter.ts
+++ packages/xforms-engine/src/integration/xpath/adapter/engineDOMAdapter.ts
@@ -1,8 +1,12 @@
 import type { XPathDOMAdapter } from '@getodk/xpath';
+import type { EngineXPathNode } from './kind.ts';
+import { getEngineXPathNodeKind, isEngineXPathNode } from './kind.ts';
 
-/**
- * @todo This will be implemented shortly! It's being added now so it can be
- * referenced by JSDoc in modules added in a prerequisite commit.
- */
-// @ts-expect-error - unknown is not assignable to XPathNode, to be updated shortly!
-export interface EngineDOMAdapter extends XPathDOMAdapter<unknown> {}
+export interface EngineDOMAdapter extends XPathDOMAdapter<EngineXPathNode> {}
+
+// @ts-expect-error - temporary to break adapter implementation into separate commits
+export const engineDOMAdapter: EngineDOMAdapter = {
+	// XPathNodeKindAdapter
+	getNodeKind: getEngineXPathNodeKind,
+	isXPathNode: isEngineXPathNode,
+};
--- packages/xforms-engine/src/integration/xpath/adapter/kind.ts
+++ packages/xforms-engine/src/integration/xpath/adapter/kind.ts
@@ -1,5 +1,13 @@
-import type { AnyParentNode, AnyValueNode } from '../../../instance/hierarchy.ts';
+import type { XPathNodeKind } from '@getodk/xpath';
+import { XPathNodeKindKey } from '@getodk/xpath';
+import type {
+	AnyChildNode,
+	AnyNode,
+	AnyParentNode,
+	AnyValueNode,
+} from '../../../instance/hierarchy.ts';
 import type { ValueContext } from '../../../instance/internal-api/ValueContext.ts';
+import type { PrimaryInstance } from '../../../instance/PrimaryInstance.ts';
 import type { ItextTranslationDefinition } from '../../../parse/model/ItextTranslation/ItextTranslationDefinition.ts';
 import type { ItextTranslationRootDefinition } from '../../../parse/model/ItextTranslation/ItextTranslationRootDefinition.ts';
 import type { SecondaryInstanceDefinition } from '../../../parse/model/SecondaryInstance/SecondaryInstanceDefinition.ts';
@@ -8,7 +16,24 @@ import type { StaticAttribute } from '../static-dom/StaticAttribute.ts';
 import type { StaticDocument } from '../static-dom/StaticDocument.ts';
 import type { StaticElement } from '../static-dom/StaticElement.ts';
 import type { StaticText } from '../static-dom/StaticText.ts';
-import type { XFormsXPathDocument, XFormsXPathElement } from './XFormsXPathNode.ts';
+import type {
+	XFormsXPathComment,
+	XFormsXPathDocument,
+	XFormsXPathElement,
+	XFormsXPathPrimaryInstanceNode,
+} from './XFormsXPathNode.ts';
+
+export type PrimaryInstanceXPathNode = Extract<AnyNode, XFormsXPathPrimaryInstanceNode>;
+
+export type PrimaryInstanceXPathElement = Extract<AnyChildNode, XFormsXPathElement>;
+
+export type PrimaryInstanceXPathComment = Extract<AnyChildNode, XFormsXPathComment>;
+
+// prettier-ignore
+export type PrimaryInstanceXPathChildNode =
+	// eslint-disable-next-line @typescript-eslint/sort-type-constituents
+	| PrimaryInstanceXPathElement
+	| PrimaryInstanceXPathComment;
 
 // prettier-ignore
 export type PrimaryInstanceXPathValueElement<Value> =
@@ -18,16 +43,23 @@ export type PrimaryInstanceXPathValueElement<Value> =
 
 // prettier-ignore
 export type EngineXPathDocument =
+	// eslint-disable-next-line @typescript-eslint/sort-type-constituents
+	| PrimaryInstance
 	| ItextTranslationDefinition
 	| SecondaryInstanceDefinition
 	| StaticDocument;
 
 // prettier-ignore
 export type EngineXPathElement =
-
+	// eslint-disable-next-line @typescript-eslint/sort-type-constituents
+	| PrimaryInstanceXPathElement
 	| ItextTranslationRootDefinition
 	| SecondaryInstanceRootDefinition
 	| StaticElement;
+
+// prettier-ignore
+export type EngineXPathComment =
+	| PrimaryInstanceXPathComment;
 
 // Giving this a type alias anticipates eventually implementing attributes
 // in primary instance state as well
@@ -37,9 +69,10 @@ export type EngineXPathText = StaticText;
 
 // prettier-ignore
 export type XFormsXPathChildNode =
-
+	// eslint-disable-next-line @typescript-eslint/sort-type-constituents
 	| EngineXPathElement
-	| EngineXPathText;
+	| EngineXPathText
+	| EngineXPathComment;
 
 // prettier-ignore
 type XPathAdapterParentNode =
@@ -65,4 +98,17 @@ export type EngineXPathNode =
 	| EngineXPathDocument
 	| EngineXPathElement
 	| EngineXPathAttribute
-	| EngineXPathText;
+	| EngineXPathText
+	| EngineXPathComment;
+
+export const isEngineXPathNode = (value: unknown): value is EngineXPathNode => {
+	return typeof value === 'object' && value != null && XPathNodeKindKey in value;
+};
+
+export const getEngineXPathNodeKind = (node: EngineXPathNode): XPathNodeKind => {
+	return node[XPathNodeKindKey];
+};
+
+export const isEngineXPathElement = (node: EngineXPathNode): node is EngineXPathElement => {
+	return getEngineXPathNodeKind(node) === 'element';
+};
