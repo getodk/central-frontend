88e6117bfc66304bc939779dad1ca04a3861576d
eyelidlessness <gnosis@gmail.com>
2024-11-21 12:09:05 -0800

    Shared abstractions for jr resources
    **JRResource:**
    
    - Represents a resource resolvable for a given `jr:` URL
    - Provides a mechanism to load the resource’s data (currently just string data, we’ll likely expand this to support media attachments)
    - Provides a helper static factory method to create a `JRResource` instance from an `XFormAttachmentFixture`
    
    **JRResourceURL:** simple subclass of standard `URL` to ensure we’re working specifically with resources associated with `jr:` URLs. This will be helpful for internals as well as engine/client APIs.
    
    **JRResourceService:**
    
    - Provides an in-memory “service” (in the sense of a server) to activate and access `JRResource`s by URL.
    
    - Activation is designed to support:
      - Current test usage (as ported from JavaRosa) where a set of fixtures are made available from their directory path, under a set of categories (“schemes” in ported JR terminology) which are ultimately used to service URLs in a pattern like `jr://$category*`
      - Anticipated test usage, where we may activate certain fixtures _inline in a specific test_. Conceptually this will be similar to inline form definitions using the JR-ported XForm DSL.
    
    - Access is designed to support a fetch-like interface. More details on this approach in a later commit introducing the engine API for clients to provide access to form attachments.
    
    - - -
    
    These abstractions are shared because it’s anticipated they’ll be used in:
    
    - `@getodk/scenario` for test setup
    - `@getodk/web-forms` for dev/demo access to fixtures
    
    We could theoretically use the same interfaces to support form attachments in the Web Forms Preview as well, i.e. to allow users to upload form definitions and their associated attachments. But we’d probably want UI/UX design to accompany that.

---
+++ packages/common/src/jr-resources/JRResourceService.ts
@@ -0,0 +1,108 @@
+import { xformAttachmentFixturesByDirectory } from '../fixtures/xform-attachments.ts';
+import { JRResource } from './JRResource.ts';
+import type { JRResourceURLString } from './JRResourceURL.ts';
+import { JRResourceURL } from './JRResourceURL.ts';
+
+export interface InlineFixtureMetadata {
+	readonly url: JRResourceURLString;
+	readonly fileName: string;
+	readonly mimeType: string;
+}
+
+class JRResourceServiceRegistry extends Map<JRResourceURLString, JRResource> {}
+
+interface ActivateFixturesOptions {
+	readonly suppressMissingFixturesDirectoryWarning?: boolean;
+}
+
+export class JRResourceService {
+	readonly resources = new JRResourceServiceRegistry();
+
+	readonly handleRequest = async (url: JRResourceURL | JRResourceURLString): Promise<Response> => {
+		let resourceKey: JRResourceURLString;
+
+		if (typeof url === 'string') {
+			resourceKey = url;
+		} else {
+			resourceKey = url.href;
+		}
+
+		const resource = this.resources.get(resourceKey);
+
+		if (resource == null) {
+			return new Response('Not found', {
+				status: 404,
+				headers: { 'Content-Type': 'text/plain' },
+			});
+		}
+
+		const { load, mimeType } = resource;
+		const body = await load();
+
+		return new Response(body, {
+			status: 200,
+			headers: { 'Content-Type': mimeType },
+		});
+	};
+
+	private setRegisteredResourceState(resource: JRResource) {
+		const url = resource.url.href;
+
+		if (this.resources.has(url)) {
+			throw new Error(`Resource already registered for URL: ${url}`);
+		}
+
+		this.resources.set(url, resource);
+	}
+
+	activateFixtures(
+		fixtureDirectory: string,
+		categories: readonly string[],
+		options?: ActivateFixturesOptions
+	): void {
+		this.reset();
+
+		try {
+			for (const category of categories) {
+				const fixtures = xformAttachmentFixturesByDirectory.get(fixtureDirectory);
+
+				if (fixtures == null) {
+					if (options?.suppressMissingFixturesDirectoryWarning !== true) {
+						// eslint-disable-next-line no-console
+						console.warn(`No form attachments in directory: ${fixtureDirectory}`);
+					}
+
+					continue;
+				}
+
+				for (const fixture of fixtures) {
+					const resource = JRResource.fromFormAttachmentFixture(category, fixture);
+
+					this.setRegisteredResourceState(resource);
+				}
+			}
+		} catch (error) {
+			// eslint-disable-next-line no-console
+			console.error('Error occurred during resource state setup:', error);
+
+			this.reset();
+		}
+	}
+
+	activateResource(metadata: InlineFixtureMetadata, data: string): void {
+		const url = JRResourceURL.from(metadata.url);
+		const load = () => Promise.resolve(data);
+		const resource = new JRResource({
+			url,
+			fileName: metadata.fileName,
+			mimeType: metadata.mimeType,
+			load,
+		});
+
+		this.setRegisteredResourceState(resource);
+	}
+
+	reset(): void {
+		this.resources.clear();
+	}
+}
