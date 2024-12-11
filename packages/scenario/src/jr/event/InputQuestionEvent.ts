eb831b3e696bec2fdc3aa70b126feb17276685a0
eyelidlessness <gnosis@gmail.com>
2024-12-11 12:57:21 -0800

    scenario: preserve runtime value type of values set with `scenario.answer`
    This helps to ensure accurate results from tests exercising casting logic.

--- packages/scenario/src/jr/event/InputQuestionEvent.ts
+++ packages/scenario/src/jr/event/InputQuestionEvent.ts
@@ -1,3 +1,4 @@
+import type { AnyInputNode, DecimalInputNode, IntInputNode } from '@getodk/xforms-engine';
 import { InputNodeAnswer } from '../../answer/InputNodeAnswer.ts';
 import { UntypedAnswer } from '../../answer/UntypedAnswer.ts';
 import type { ValueNodeAnswer } from '../../answer/ValueNodeAnswer.ts';
@@ -8,11 +9,47 @@ export class InputQuestionEvent extends QuestionEvent<'input'> {
 		return new InputNodeAnswer(this.node);
 	}
 
-	answerQuestion(answerValue: unknown): ValueNodeAnswer {
+	private answerDefault(node: AnyInputNode, answerValue: unknown): ValueNodeAnswer {
 		const { stringValue } = new UntypedAnswer(answerValue);
 
-		this.node.setValue(stringValue);
+		node.setValue(stringValue);
 
-		return new InputNodeAnswer(this.node);
+		return new InputNodeAnswer(node);
+	}
+
+	private answerNumericQuestionNode(
+		node: DecimalInputNode | IntInputNode,
+		answerValue: unknown
+	): ValueNodeAnswer {
+		if (answerValue === null) {
+			node.setValue(answerValue);
+
+			return new InputNodeAnswer(node);
+		}
+
+		switch (typeof answerValue) {
+			case 'bigint':
+			case 'number':
+			case 'string':
+				node.setValue(answerValue);
+
+				return new InputNodeAnswer(node);
+
+			default:
+				return this.answerDefault(node, answerValue);
+		}
+	}
+
+	answerQuestion(answerValue: unknown): ValueNodeAnswer {
+		const { node } = this;
+
+		switch (node.valueType) {
+			case 'int':
+			case 'decimal':
+				return this.answerNumericQuestionNode(node, answerValue);
+
+			default:
+				return this.answerDefault(node, answerValue);
+		}
 	}
 }
