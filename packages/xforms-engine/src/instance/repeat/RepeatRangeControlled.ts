bf1d4a63675799ebb7cd665ce40649c84363168f
eyelidlessness <gnosis@gmail.com>
2025-03-14 10:03:47 -0700

    engine: add warning for dropping excess repeat instance input nodes
    Note: this is rework of several earlier efforts to warn under the same conditions (and skip warning under the same excluded conditions). This revision of the change addresses:
    
    1. The fact that upstream commits have vastly simplified some other repeat-related changes where the logic was introduced in prior iterations.
    
    2. The fact that when warning occurs (and why that when applies) was much less clear than it could have been, especially once integrated with much simpler upstream changes. (At least in the source; it’s a bit more clear in the tests…)

--- packages/xforms-engine/src/instance/repeat/RepeatRangeControlled.ts
+++ packages/xforms-engine/src/instance/repeat/RepeatRangeControlled.ts
@@ -1,4 +1,5 @@
 import { createComputed } from 'solid-js';
+import type { LoadFormWarnings } from '../../client/form/LoadFormResult.ts';
 import type { RepeatRangeNodeAppearances } from '../../client/repeat/BaseRepeatRangeNode.ts';
 import type { RepeatRangeControlledNode } from '../../client/repeat/RepeatRangeControlledNode.ts';
 import type { AncestorNodeValidationState } from '../../client/validation.ts';
@@ -11,10 +12,18 @@ import type { GeneralParentNode } from '../hierarchy.ts';
 import type { EvaluationContext } from '../internal-api/EvaluationContext.ts';
 import { BaseRepeatRange } from './BaseRepeatRange.ts';
 
+// prettier-ignore
+type RepeatCountControlType =
+	| 'REPEAT_COUNT_CONTROL_DYNAMIC'
+	| 'REPEAT_COUNT_CONTROL_FIXED';
+
 export class RepeatRangeControlled
 	extends BaseRepeatRange<ControlledRepeatDefinition>
 	implements RepeatRangeControlledNode, XFormsXPathNodeRange, EvaluationContext
 {
+	private readonly isInstanceCreation: boolean;
+	private readonly countControlType: RepeatCountControlType;
+
 	// RepeatRangeControlledNode
 	readonly nodeType = 'repeat-range:controlled';
 	readonly appearances: RepeatRangeNodeAppearances;
@@ -27,7 +36,14 @@ export class RepeatRangeControlled
 	) {
 		super(parent, definition);
 
+		this.isInstanceCreation = parent.rootDocument.initializationMode === 'create';
 		this.appearances = definition.bodyElement.appearances;
+
+		if (definition.count.isConstantExpression()) {
+			this.countControlType = 'REPEAT_COUNT_CONTROL_FIXED';
+		} else {
+			this.countControlType = 'REPEAT_COUNT_CONTROL_DYNAMIC';
+		}
 
 		this.initializeControlledChildrenState(definition, instanceNodes);
 
@@ -45,7 +61,9 @@ export class RepeatRangeControlled
 				defaultValue: 0,
 			});
 
-			createComputed<number>((previousCount) => {
+			createComputed((previousCountComputation: number | null = null): number => {
+				const previousCount = previousCountComputation ?? 0;
+
 				let currentCount = computeCount();
 
 				if (Number.isFinite(currentCount) && currentCount < 0) {
@@ -63,6 +81,8 @@ export class RepeatRangeControlled
 				) {
 					return previousCount;
 				}
+
+				this.warnDroppedInstanceNodes(repeatInstanceNodes, previousCountComputation, currentCount);
 
 				if (currentCount > previousCount) {
 					const delta = currentCount - previousCount;
@@ -82,7 +102,54 @@ export class RepeatRangeControlled
 				}
 
 				return currentCount;
-			}, 0);
+			});
 		});
+	}
+
+	/**
+	 * Note: we do not warn about dropping "excess" repeat instances...
+	 *
+	 * - ... after the instance is fully loaded: dropping repeat instances after
+	 *   that point is an expected behavior of _dynamically-controlled repeats_
+	 *   (i.e. those defined with `jr:count`), and it does not occur for
+	 *   _fixed-controlled repeats_ (i.e. defined with `jr:noAddRemove`);
+	 *
+	 * - ... for dynamic (`jr:count`) repeats _in newly created form instances_, i.e. instances whose input instance
+	 *   nodes are derived from the _form definition_: those form-defined repeat
+	 *   instances are not "dropped", instead they're retained in memory to be
+	 *   added as count is recomputed to a larger value.
+	 *
+	 * @todo ^ it is pretty likely that retain-in-memory behavior also applies to
+	 * instance state which has been serialized and restored as well! In this way,
+	 * it's possible we're not really "dropping" those nodes, only deferring their
+	 * restoration. Is this what we want?
+	 *
+	 * @todo Design and produce {@link LoadFormWarnings | warnings} as values!
+	 */
+	private warnDroppedInstanceNodes(
+		repeatInstanceNodes: readonly StaticElement[],
+		previousCountComputation: number | null,
+		initialCount: number
+	): void {
+		// See this method's JSDoc notes for reasoning!
+		if (this.isInstanceCreation) {
+			return;
+		}
+
+		// See this method's JSDoc notes for reasoning!
+		const isInstanceLoad = previousCountComputation == null;
+
+		if (isInstanceLoad && this.countControlType === 'REPEAT_COUNT_CONTROL_DYNAMIC') {
+			return;
+		}
+
+		const droppedCount = repeatInstanceNodes.length - initialCount;
+
+		if (droppedCount > 0) {
+			// eslint-disable-next-line no-console
+			console.warn(
+				`Dropped ${droppedCount} repeat instances for repeat range ${this.contextReference()}`
+			);
+		}
 	}
 }
