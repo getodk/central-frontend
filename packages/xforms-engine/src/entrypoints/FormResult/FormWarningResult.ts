getodk/web-forms

engine: entrypoint interface overhaul, split form load and instance creation
This addresses a few overlapping concerns.

**Engine support for #316: Prepare a new instance for filling from an active form definition and form attachments**

A client can now load a form (definition, related resources such as external secondary instances) once and create any number of instances from the result. This is handled by splitting the interface for those aspects of what was formerly named `initializeForm` into:

1. `loadForm`, which now produces a `LoadFormResult`.
2. `createInstance`, exposed to clients as both:
    - A contextualized member of `LoadFormResult`
    - A convenience entrypoint composing steps 1 & 2 (in both its signature and behavior)

**Lay groundwork for edit-oriented equivalents to `createInstance`**

In subsequent commits, we’ll expose additional entrypoints for editing instances. Those additional entrypoints will follow the same pattern as `createInstance`, both as members of `LoadFormResult` and as composed convenience entrypoints.

**General cleanup of related types, documentation, exports, package structure**

This is a fairly broad category, and it accounts for the vast majority of diff noise in this commit. The vast majority is related to either refining terminology, or usage flow changes. There’s also some opportunistic cleanup of structure. Hopefully this is all pretty straightforward in review at the commit level!

- - -

**A couple additional notes on `LoadFormResult`**

- This interface continues to keep the engine’s “form” concepts relatively private from clients (except for the parts we already over-expose as node `definition`s. I expect we’ll expand the interface over time, exposing more about a form as it’s parsed and resolved. I’ve deferred that for now, in favor of putting more thought and intention into how we expose any still-internal concepts and/or structures.

- The interface _is not_ a great candidate for exposing certain mid-parse deta

packages/xforms-engine/src/entrypoints/FormResult/FormWarningResult.ts
@@ -1,0 +1,25 @@
+import type { LoadFormWarningResult, LoadFormWarnings } from '../../client/form/LoadFormResult.ts';
+import type { FormResource } from '../../instance/resource.ts';
+import type { ReactiveScope } from '../../lib/reactivity/scope.ts';
+import type { FormInstanceBaseOptions } from '../FormInstance.ts';
+import { BaseInstantiableFormResult } from './InstantiableFormResult.ts';
+
+export interface FormWarningResultOptions {
+	readonly warnings: LoadFormWarnings;
+	readonly error: null;
+	readonly scope: ReactiveScope;
+	readonly formResource: FormResource;
+	readonly instanceOptions: FormInstanceBaseOptions;
+}
+
+export class FormWarningResult
+	extends BaseInstantiableFormResult<'warning'>
+	implements LoadFormWarningResult
+{
+	constructor(options: FormWarningResultOptions) {
+		super({
+			status: 'warning',
+			...options,
+		});
+	}
+}

getodk/central-frontend

packages/xforms-engine/src/entrypoints/FormResult/FormWarningResult.ts
@@ -1,0 +1,25 @@
+import type { LoadFormWarningResult, LoadFormWarnings } from '../../client/form/LoadFormResult.ts';
+import type { FormResource } from '../../instance/resource.ts';
+import type { ReactiveScope } from '../../lib/reactivity/scope.ts';
+import type { FormInstanceBaseOptions } from '../FormInstance.ts';
+import { BaseInstantiableFormResult } from './BaseInstantiableFormResult.ts';
+
+export interface FormWarningResultOptions {
+	readonly warnings: LoadFormWarnings;
+	readonly error: null;
+	readonly scope: ReactiveScope;
+	readonly formResource: FormResource;
+	readonly instanceOptions: FormInstanceBaseOptions;
+}
+
+export class FormWarningResult
+	extends BaseInstantiableFormResult<'warning'>
+	implements LoadFormWarningResult
+{
+	constructor(options: FormWarningResultOptions) {
+		super({
+			status: 'warning',
+			...options,
+		});
+	}
+}