c72a76b112f69a4447375f84734133f67c887404
Sadiq Khoja <sadiqkhoja@gmail.com>
2025-04-07 19:09:48 -0400

    Enhances getodk/central#942: ability to redirect to external sites

--- test/components/enketo-iframe.spec.js
+++ test/components/enketo-iframe.spec.js
@@ -56,11 +56,11 @@ describe('EnketoIframe', () => {
     iframe.attributes('src').should.contain(`${encodeURIComponent('d[/data/first_name]')}=john`);
   });
 
-  it('redirects on submissionsuccess message with return_url', async () => {
+  it('redirects on submissionsuccess message with return_url - internal', async () => {
     const wrapper = mountComponent({
-      props: { enketoId, actionType: 'new', instanceId: 'test-instance' },
+      props: { enketoId, actionType: '', instanceId: 'test-instance' },
       container: {
-        router: mockRouter('/?return_url=http%3A%2F%2Flocalhost%2Fprojects%2F1')
+        router: mockRouter(`/?return_url=${window.location.origin}/projects/1`)
       }
     });
     const iframe = wrapper.find('iframe');
@@ -74,6 +74,33 @@ describe('EnketoIframe', () => {
     await wait();
 
     wrapper.vm.$router.push.calledWith('/projects/1').should.be.true;
+  });
+
+  it('redirects on submissionsuccess message with return_url - external', async () => {
+    const fakeAssign = sinon.fake();
+    const wrapper = mountComponent({
+      props: { enketoId, actionType: '', instanceId: 'test-instance' },
+      container: {
+        router: mockRouter('/?return_url=http://example.com/projects/1'),
+        location: {
+          origin: window.location.origin,
+          assign: (url) => {
+            fakeAssign(url);
+          }
+        }
+      }
+    });
+    const iframe = wrapper.find('iframe');
+
+    const script = document.createElement('script');
+    script.textContent = `
+      window.parent.postMessage(JSON.stringify({ enketoEvent: 'submissionsuccess' }), "*");
+    `;
+    iframe.element.contentDocument.body.appendChild(script);
+
+    await wait();
+
+    fakeAssign.calledWith(new URL('http://example.com/projects/1')).should.be.true;
   });
 
   it('bubbles up the message event', async () => {
