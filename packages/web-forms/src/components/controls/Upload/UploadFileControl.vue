46c52bab0737b5ba64f2a64ba596d29e27aa1efa
eyelidlessness <gnosis@gmail.com>
2025-04-11 16:21:29 -0700

    PR feedback: this is more valuable as social/prioritization context

--- packages/web-forms/src/components/controls/Upload/UploadFileControl.vue
+++ packages/web-forms/src/components/controls/Upload/UploadFileControl.vue
@@ -9,10 +9,5 @@ defineProps<UploadFileControlProps>();
 </script>
 
 <template>
-	<!--
-		It would probably have taken minimal work to support the general (non-image)
-		case for <upload> controls, but the ask was to not do that.
-	-->
-
 	<p>🚧 Support for non-image uploads is pending. 🚧</p>
 </template>
