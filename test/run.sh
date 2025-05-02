4f50fc1d0ac6c1e0fc1f6c712f76b9eec070aaf2
Kathleen Tuite <ktuite@getodk.org>
2025-05-02 16:37:17 -0700

    Reverting parts of this PR that show up in another PR

--- test/run.sh
+++ test/run.sh
@@ -5,12 +5,11 @@ output=$(mktemp)
 trap 'rm -- "$output"' EXIT
 NODE_ENV=test karma start | tee "$output"
 # Search for: warnings from console.warn(), including Vue warnings; Sass
-# warnings; and warnings from Karma. Ignore build warnings
-if awk '/Connected on socket/ {found=1; next} found' "$output" | \
-	grep -F -e 'WARN LOG:' -e 'ERROR LOG:' -e 'Module Warning' -e 'WARN [web-server]:' -C5 "$output"; then
-		# Reset the text format in case the search results contained formatted text.
-		tput sgr0
-		echo
-		echo 'All tests passed, but there were warnings: see above.'
-		exit 1
+# warnings; and warnings from Karma.
+if grep -F -e 'WARN LOG:' -e 'ERROR LOG:' -e 'Module Warning' -e 'WARN [web-server]:' -C5 "$output"; then
+	# Reset the text format in case the search results contained formatted text.
+	tput sgr0
+	echo
+	echo 'All tests passed, but there were warnings: see above.'
+	exit 1
 fi
