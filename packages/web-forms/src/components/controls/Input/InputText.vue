81d254054df3e6bb8fe1b3b3139fe023cd18bf66
Jennifer Q <66472237+latin-panda@users.noreply.github.com>
2025-05-13 17:27:37 -0600

    feat(#301): Add support for a multiline text question type. (#402)

--- packages/web-forms/src/components/controls/Input/InputText.vue
+++ packages/web-forms/src/components/controls/Input/InputText.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import type { StringInputNode, TemporaryStringValueInputNode } from '@getodk/xforms-engine';
 import InputText from 'primevue/inputtext';
+import Textarea from 'primevue/textarea';
 import { computed, inject } from 'vue';
 
 // prettier-ignore
@@ -21,23 +22,50 @@ const setValue = (value = '') => {
 const doneAnswering = inject<boolean>('doneAnswering');
 const submitPressed = inject<boolean>('submitPressed');
 const invalid = computed(() => props.node.validationState.violation?.valid === false);
+const rows = computed(() => {
+	const options = props.node.nodeOptions;
+	if (options && 'rows' in options) {
+		return options.rows ?? 0;
+	}
+	return 0;
+});
 </script>
 
 <template>
-	<InputText
-		:id="node.nodeId"
-		:required="node.currentState.required"
-		:disabled="node.currentState.readonly"
-		:class="{'inside-highlighted': invalid && submitPressed}"
-		:model-value="node.currentState.value"
-		@update:model-value="setValue"
-		@input="doneAnswering = false"
-		@blur="doneAnswering = true"
-	/>
+	<template v-if="rows > 0">
+		<Textarea
+			:id="node.nodeId"
+			:required="node.currentState.required"
+			:disabled="node.currentState.readonly"
+			:class="{'inside-highlighted': invalid && submitPressed}"
+			:model-value="node.currentState.value"
+			:rows="rows"
+			@update:model-value="setValue"
+			@input="doneAnswering = false"
+			@blur="doneAnswering = true"
+		/>
+	</template>
+	<template v-else>
+		<InputText
+			:id="node.nodeId"
+			:required="node.currentState.required"
+			:disabled="node.currentState.readonly"
+			:class="{'inside-highlighted': invalid && submitPressed}"
+			:model-value="node.currentState.value"
+			@update:model-value="setValue"
+			@input="doneAnswering = false"
+			@blur="doneAnswering = true"
+		/>
+	</template>
 </template>
 
 <style scoped>
 .p-inputtext {
 	width: 100%;
+}
+
+.p-textarea {
+	width: 100%;
+	resize: none;
 }
 </style>
