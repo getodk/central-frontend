eaa05a15a87f1f135d365fb866377265ba8d9bf0
alxndrsn <alxndrsn>
2025-08-15 07:14:09 +0000

    wip

--- bin/check-bundle-size.js
+++ bin/check-bundle-size.js
@@ -3,12 +3,14 @@
 /* eslint-disable array-bracket-spacing, indent, key-spacing, no-confusing-arrow, no-console, no-else-return, no-multi-spaces, no-plusplus, no-use-before-define, prefer-template, semi-style */
 
 const { execSync } = require('node:child_process');
-const { extname } = require('node:path');
+const { basename, extname } = require('node:path');
 
 const log = (...args) => console.log('[check-bundle-size]', ...args);
 
-const minSize = 5000000;
-const maxSize = 6000000;
+const enableColours = !!process.env.COLORTERM;
+const RED  =  '\x1b[31m';
+const BOLD =  '\x1b[1m';
+const RESET = '\x1b[0m';
 
 log('---');
 log('Individual file sizes:');
@@ -24,8 +26,10 @@ const files = execSync('find dist/ -type f -exec du -b {} \\;', { encoding:'utf8
     })
     .sort((a, b) => a.path > b.path ? 1 : -1)
     .map(f => {
-      log(f.size.toString().padStart(7, ' '), f.path);
-      return { ...f, ok:isSizeOk(f) };
+      const tooBig = isTooBig(f);
+      const sizeText = (tooBig ? redBold : normal)(f.size.toString().padStart(7, ' '));
+      log(sizeText, f.path);
+      return { ...f, tooBig };
     })
     ;
 
@@ -33,7 +37,7 @@ log('---');
 log('  files:', files.length);
 log('---');
 
-const tooBigs = files.filter(f => !f.ok);
+const tooBigs = files.filter(f => f.tooBig);
 if (tooBigs.length) {
   log('!!!');
   log(`!!! ${tooBigs.length} file(s) are too big !!!`);
@@ -51,7 +55,32 @@ function humanSize(bytes) {
   else                return bytes + ' B';
 }
 
-function isSizeOk({ path, size }) {
+function isTooBig({ path, size }) {
+  // Special cases:
+  const simpleName = basename(path).replace(/-\w{8}\./, '.');
+  switch(simpleName) {
+    case 'index.js':             return size >   450_000;
+    case 'web-form-renderer.js': return size > 3_000_000;
+  }
+
   const type = extname(path).substr(1);
-  return true;
+  switch(type) {
+    case 'css':  return size > 220_000;
+    case 'html': return size >   1_200;
+    case 'ico':  return size >  16_000;
+    case 'js':   return size > 200_000;
+    case 'png':  return size >  40_000;
+    case 'svg':  return size >  60_000;
+    case 'ttf':  return size >  18_000;
+    case 'woff': return size >  18_000;
+  }
+  throw new Error(`No check written for file ${path} yet!  Please review this function.`);
+}
+
+function redBold(text) {
+  if(!enableColours) return text;
+  return [ RED, BOLD, text, RESET ].join('');
+}
+function normal(text) {
+  return text;
 }
