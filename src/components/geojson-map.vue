c459bdffe424c0a081956ad16970dd751d4cba7c
Matthew White <matthew-white@users.noreply.github.com>
2025-10-21 00:28:45 -0400

    Add v-show around .control-bar

--- src/components/geojson-map.vue
+++ src/components/geojson-map.vue
@@ -12,13 +12,15 @@ except according to the terms contained in the LICENSE file.
 <template>
   <div v-show="featureCount !== 0" ref="el" class="geojson-map">
     <div ref="mapContainer" class="map-container" :class="{ opaque: shown }" tabindex="0" :inert="!shown"></div>
-    <span v-show="shown" class="count">{{ countMessage }}</span>
-    <div class="control-bar">
-      <button v-tooltip.aria-describedby="$t('zoomToFit')" type="button"
-        @click="fitViewToAllFeatures">
-        <!-- eslint-disable-next-line vuejs-accessibility/alt-text -->
-        <img class="fit-icon" :src="FitIcon">
-      </button>
+    <div v-show="shown">
+      <span class="count">{{ countMessage }}</span>
+      <div class="control-bar">
+        <button v-tooltip.aria-describedby="$t('zoomToFit')" type="button"
+          @click="fitViewToAllFeatures">
+          <!-- eslint-disable-next-line vuejs-accessibility/alt-text -->
+          <img class="fit-icon" :src="FitIcon">
+        </button>
+      </div>
     </div>
   </div>
 </template>
