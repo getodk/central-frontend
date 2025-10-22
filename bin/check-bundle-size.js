a5949e65e2d82217b9e23042d1646c0c7a5458cb
Jennifer Q <66472237+latin-panda@users.noreply.github.com>
2025-10-23 01:00:27 +0700

    chore(#1223): upgrades to web-forms v0.16.0 (#1378)
    Co-authored-by: Matthew White <matthew-white@users.noreply.github.com>

--- bin/check-bundle-size.js
+++ bin/check-bundle-size.js
@@ -55,10 +55,11 @@ log('File sizes look OK.');
 
 function isTooBig({ path, size }) {
   // Special cases:
-  const simpleName = basename(path).replace(/-[\w-]{8}\./, '.');
+  const simpleName = basename(path).replace(/(-[\w-]{8})+\./, '.');
   switch (simpleName) { // eslint-disable-line default-case
     case 'index.js':             return size >   450_000;
-    case 'web-form-renderer.js': return size > 3_000_000;
+    case 'web-form.js': return size > 3_300_000;
+    case 'MapBlock.js': return size > 450_000; // A Web Forms' feature bundle
     case 'geojson-map.js':       return size >   500_000;
   }
 
