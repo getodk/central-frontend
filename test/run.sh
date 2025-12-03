0738754583d8fea7f052f27764f3ae4ac0b11b1c
alxndrsn <alxndrsn>
2025-12-03 13:28:58 +0000

    ci: add linting, shellcheck for bash scripts
    Introduces https://github.com/getodk/central/blob/master/test/check-scripts.sh, with two changes:
    
    * disables tab/space indentation enforcement, as this repo currently uses both
    * ignores SC2038, as `find` does not generate exit codes in the same way as `xargs`, and current code will fail obviously if files are introduced with names which confuse xargs
    * does not ignore SC2016, as this repo does not have similar variable substitution to the odk-central repo
    
    Closes #1513

--- test/run.sh
+++ test/run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -eu
 set -o pipefail
+shopt -s inherit_errexit
 
 # Normally, index.html is housed at the root of the repository for Vite, but
 # here we move it to public/, where Vue CLI expects it.
@@ -27,14 +28,14 @@ for pattern in "$@"; do
 	# warnings; and warnings from Karma.
 	set -- -F -e 'WARN LOG:' -e 'ERROR LOG:' -e 'Module Warning' -e 'WARN [web-server]:' "$output"
 	warnings=$(grep -c "$@")
-	if (( $warnings > 2)); then
+	if (( warnings > 2)); then
 		grep -C5 "$@"
 		# Reset the text format in case the search results contained formatted text.
 		tput sgr0
 		echo
 		echo "All tests passed, but there were $warnings warnings: see above."
 		exit 1
-	elif (( $warnings > 0 )); then
+	elif (( warnings > 0 )); then
 		echo "There were $warnings warnings, which is within the accepted threshold."
 	fi
 
