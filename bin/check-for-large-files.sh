67b6415fc0a1884877867e927d3ba70b88949a39
alxndrsn <alxndrsn>
2025-12-16 08:14:10 +0000

    update script with central repo changes

--- bin/check-for-large-files.sh
+++ bin/check-for-large-files.sh
@@ -1,22 +1,25 @@
 #!/bin/bash -eu
-git ls-files -z | \
-    xargs -0 ls -l |
-    awk '
-      BEGIN {
-        print "[check-for-large-files] Checking for large files...";
-      }
+set -o pipefail
+shopt -s inherit_errexit
 
-      $5 > 1000000 {
-        ++n;
-        print $5 "\t" $9;
-      }
+git ls-files -z \
+| xargs -0 ls -l \
+| awk '
+  BEGIN {
+    print "[check-for-large-files] Checking for large files...";
+  }
 
-      END {
-        if(n>0) {
-          print "[check-for-large-files] !!! " n " LARGE FILE(S) FOUND";
-          exit 1;
-        } else {
-          print "[check-for-large-files] No large files found ✅";
-        }
-      }
-    '
+  $5 > 1000000 {
+    ++n;
+    print $5 "\t" $9;
+  }
+
+  END {
+    if(n>0) {
+      print "[check-for-large-files] !!! " n " LARGE FILE(S) FOUND";
+      exit 1;
+    } else {
+      print "[check-for-large-files] No large files found ✅";
+    }
+  }
+'
