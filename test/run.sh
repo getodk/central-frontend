ecedb7abc3f1707fc38176b12522ebea2b839277
alxndrsn <alxndrsn>
2026-01-13 17:19:07 +0000

    hellcheck

--- test/run.sh
+++ test/run.sh
@@ -14,13 +14,13 @@ NODE_ENV=test karma start | tee "$output"
 # warnings; and warnings from Karma.
 set -- -F -e 'WARN LOG:' -e 'ERROR LOG:' -e 'Module Warning' -e 'WARN [web-server]:' "$output"
 warnings=$(grep -c "$@")
-if (( $warnings > 2)); then
+if (( warnings > 2)); then
   grep -C5 "$@"
   # Reset the text format in case the search results contained formatted text.
   tput sgr0
   echo
   echo "All tests passed, but there were $warnings warnings: see above."
   exit 1
-elif (( $warnings > 0 )); then
+elif (( warnings > 0 )); then
   echo "There were $warnings warnings, which is within the accepted threshold."
 fi
