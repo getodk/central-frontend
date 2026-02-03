44699b4663f72b2e610e827db550c5727275d496
alxndrsn <alxndrsn>
2026-02-03 09:47:38 +0000

    revert submission list timeout

--- test/components/submission/list.spec.js
+++ test/components/submission/list.spec.js
@@ -817,7 +817,7 @@ describe('SubmissionList', () => {
   });
 
   describe('pagination', function() { // eslint-disable-line func-names
-    this.timeout(16000);
+    this.timeout(4000);
 
     const checkIds = (component, count, offset = 0) => {
       const rows = component.findAllComponents(SubmissionDataRow);
