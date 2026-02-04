74601324b971a91464a30093602c88f29fd5c794
alxndrsn <alxndrsn>
2026-02-04 10:05:31 +0000

    use awk to support count 0

--- test/run.sh
+++ test/run.sh
@@ -12,14 +12,17 @@ NODE_ENV="test" karma start | tee "$output"
 
 # Search for: warnings from console.warn(), including Vue warnings; Sass
 # warnings; and warnings from Karma.
-set -- -F -e 'WARN LOG:' -e 'ERROR LOG:' -e 'Module Warning' -e 'WARN [web-server]:' "$output"
-warnings=$(grep -c "$@")
-if (( warnings > 2)); then
-  echo "----- WARNINGS: -----"
-  grep -C5 "$@"
-  echo
-  echo "All tests passed, but there were $warnings warnings: see above."
-  exit 1
-fi
-
-echo "There were $warnings warnings, which is within the accepted threshold."
+awk '
+  BEGIN { count = 0 }
+  /WARN LOG:/          { ++count }
+  /ERROR LOG:/         { ++count }
+  /Module Warning/     { ++count }
+  /WARN [web-server]:/ { ++count }
+  END {
+    if(count > 2) {
+      print "All tests passed, but there were " warnings " warnings: see above."
+      exit 1
+    }
+    echo "There were " warnings " warnings, which is within the accepted threshold."
+  }
+' "$output"
