5dc12ddd82f6481b1c1caddf6a6b4399846fcf85
alxndrsn <alxndrsn>
2026-02-05 00:46:08 +0000

    karma: revert browserNoActivityTimeout change
    Attempting to find the minimal change needed to karma config to fix test robustness.

--- karma.conf.js
+++ karma.conf.js
@@ -68,7 +68,7 @@ module.exports = (config) => {
     //browserSocketTimeout: 210000,
     //browserDisconnectTimeout: 210000,
     //browserDisconnectTolerance: 3,
-    browserNoActivityTimeout: 210000,
+    //browserNoActivityTimeout: 210000,
 
     reporters: ['spec'],
     singleRun: true,
