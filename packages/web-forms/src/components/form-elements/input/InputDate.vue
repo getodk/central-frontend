975049f649e7b02e4bc079ee5da53c326814b100
Jennifer Q <66472237+latin-panda@users.noreply.github.com>
2026-04-24 21:07:40 +0700

    feat(#782): add support for "month-year" and "year" appearances (#786)

--- packages/web-forms/src/components/form-elements/input/InputDate.vue
+++ packages/web-forms/src/components/form-elements/input/InputDate.vue
@@ -8,6 +8,9 @@ import { useDateTimeInput } from './useDateTimeInput.ts';
 const props = defineProps<{ readonly question: DateInputNode }>();
 const { localeDateFormat, getTemporalString } = useDateTimeInput();
 
+const isMonthYear = computed(() => props.question.appearances['month-year']);
+const isYearOnly = computed(() => props.question.appearances.year);
+
 const value = computed({
 	get: () => {
 		const temporalValue = getTemporalString(props.question.currentState.value, ISO_DATE_LIKE_PATTERN);
@@ -15,8 +18,40 @@ const value = computed({
 		return temporalValue === null ? null : new Date(temporalValue + 'T00:00:00');
 	},
 	set: (newDate) => {
+		if (newDate != null) {
+			if (isMonthYear.value || isYearOnly.value) {
+				newDate.setDate(1);
+			}
+
+			if (isYearOnly.value) {
+				newDate.setMonth(0);
+			}
+		}
 		props.question.setValue(newDate);
 	},
+});
+
+// PrimeVue has its date format convention, for example, 'yy' = 4-digit year
+const pickerConfig = computed(() => {
+	const hideTodayClass = 'hide-today-button';
+	if (isMonthYear.value) {
+		return {
+			view: 'month',
+			dateFormat: 'MM yy',
+			placeholder: 'mmm yyyy',
+			panelClass: hideTodayClass
+		};
+	}
+
+	if (isYearOnly.value) {
+		return { view: 'year', dateFormat: 'yy', placeholder: 'yyyy', panelClass: hideTodayClass };
+	}
+
+	return {
+		view: 'date',
+		dateFormat: undefined, // Fallback to PrimeVue locale default
+		placeholder: localeDateFormat.value,
+	};
 });
 
 const isDisabled = computed(() => props.question.currentState.readonly === true);
@@ -25,9 +60,12 @@ const isDisabled = computed(() => props.question.currentState.readonly === true)
 <template>
 	<DatePicker
 		v-model="value"
+		:view="pickerConfig.view"
+		:date-format="pickerConfig.dateFormat"
 		show-icon
 		icon-display="input"
-		:placeholder="localeDateFormat"
+		:panel-class="pickerConfig.panelClass"
+		:placeholder="pickerConfig.placeholder"
 		show-button-bar
 		:disabled="isDisabled"
 	/>
