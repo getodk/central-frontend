9c8e7fdb9d54e7f69780889d95d19fbf09d2eb0d
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2026-05-13 14:13:15 +1200

    get central frontend testing working

--- apps/central/karma.conf.js
+++ apps/central/karma.conf.js
@@ -56,29 +56,28 @@ webpackConfigForKarma.module.rules.push({
 
 module.exports = (config) => {
   config.set({
-    basePath: '../../',
     frameworks: ['webpack', 'mocha', 'source-map-support'],
     files: [
-      'apps/central/test/index.js',
-      { pattern: 'public/fonts/icomoon.ttf', served: true, included: false },
-      { pattern: 'public/blank.html', served: true, included: false },
-      { pattern: 'apps/central/test/files/*', served: true, included: false },
-      { pattern: 'apps/central/src/assets/images/**', served: true, included: false }
+      'test/index.js',
+      { pattern: '../../public/fonts/icomoon.ttf', served: true, included: false },
+      { pattern: '../../public/blank.html', served: true, included: false },
+      { pattern: 'test/files/*', served: true, included: false },
+      { pattern: 'src/assets/images/**', served: true, included: false }
     ],
     proxies: {
-      '/fonts/': '/base/public/fonts/',
-      '/blank.html': '/base/public/blank.html',
-      '/test/files/': '/base/apps/central/test/files/',
+      '/fonts/': '../../public/fonts/',
+      '/blank.html': '../../public/blank.html',
+      '/test/files/': '/base/test/files/',
 
       // Images
-      '/v1/config/public/hero-image': '/base/apps/central/src/assets/images/whats-new/banner@1x.png',
-      '/v1/config/public/logo': '/base/apps/central/src/assets/images/odk-logo.png',
-      '/img/banner@1x.6c9e9f21.png': '/base/apps/central/src/assets/images/whats-new/banner@1x.png', // Smaller resolution for circleCI test
-      '/img/map-location.b523ce2d.svg': '/base/apps/central/src/assets/images/geojson-map/map-location.svg',
-      '/img/fullscreen.37a932a6.svg': '/base/apps/central/src/assets/images/geojson-map/fullscreen.svg'
+      '/v1/config/public/hero-image': '/base/src/assets/images/whats-new/banner@1x.png',
+      '/v1/config/public/logo': '/base/src/assets/images/odk-logo.png',
+      '/img/banner@1x.6c9e9f21.png': '/base/src/assets/images/whats-new/banner@1x.png', // Smaller resolution for circleCI test
+      '/img/map-location.b523ce2d.svg': '/base/src/assets/images/geojson-map/map-location.svg',
+      '/img/fullscreen.37a932a6.svg': '/base/src/assets/images/geojson-map/fullscreen.svg'
     },
     preprocessors: {
-      'apps/central/test/index.js': ['webpack', 'sourcemap']
+      'test/index.js': ['webpack', 'sourcemap']
     },
     webpack: webpackConfigForKarma,
     browsers: ['ChromeHeadless'],
