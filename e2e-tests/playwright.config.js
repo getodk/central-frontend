4cbb71fdf11ee21362bee76a4bfc397f3f197550
alxndrsn <alxndrsn>
2026-06-05 12:26:02 +0000

    ci/e2e-tests: bail after first failure
    This should lead to quicker pipeline failure, and so faster feedback for developers.
    
    Ref https://github.com/getodk/central/issues/1923

--- e2e-tests/playwright.config.js
+++ e2e-tests/playwright.config.js
@@ -11,6 +11,8 @@ export default defineConfig({
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+  /* Bail after first test failure in CI */
+  maxFailures: process.env.CI ? 1 : undefined,
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
   /* Opt out of parallel tests on CI. */
