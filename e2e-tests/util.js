e51a4b39a2428f88b6411217f6b5a46d8ba09eb6
alxndrsn <alxndrsn>
2026-06-05 11:48:49 +0000

    msg.text

--- e2e-tests/util.js
+++ e2e-tests/util.js
@@ -30,7 +30,7 @@ const test = testBase.extend({
           for(const arg of msg.args()) args.push(await arg.jsonValue());
         } catch(err) {
           if(err.message !== 'Execution context was destroyed, most likely because of a navigation') throw err;
-          args.push(page.text());
+          args.push(msg.text());
         }
 
         console.log(
