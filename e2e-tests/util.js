f5b9901f25ac8b8128ed7377efca7cbcaa5bc149
alxndrsn <alxndrsn>
2026-06-05 11:48:36 +0000

    page.text()

--- e2e-tests/util.js
+++ e2e-tests/util.js
@@ -25,7 +25,13 @@ const test = testBase.extend({
         const { url, line, column } = msg.location();
 
         const args = [];
-        for(const arg of msg.args()) args.push(await arg.jsonValue());
+
+        try {
+          for(const arg of msg.args()) args.push(await arg.jsonValue());
+        } catch(err) {
+          if(err.message !== 'Execution context was destroyed, most likely because of a navigation') throw err;
+          args.push(page.text());
+        }
 
         console.log(
           `[${browserName}] [console.${msg.type()}] ${url}:${line}:${column}` +
