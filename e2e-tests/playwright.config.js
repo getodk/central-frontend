2296a04ed13dd2f3aa303134b0c3b6b2b9c4d87a
Alex Anderson <191496+alxndrsn@users.noreply.github.com>
2026-06-06 08:09:28 +0300

    ci/e2e-tests: do not retry failed tests (#1603)
    Tests are not currently flaky, so retrying failed tests just wastes time and hides new issues.
    
    Ref https://github.com/getodk/central/issues/1923

--- e2e-tests/playwright.config.js
+++ e2e-tests/playwright.config.js
@@ -19,8 +19,8 @@ export default defineConfig({
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  /* Do not retry failed tests. */
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
