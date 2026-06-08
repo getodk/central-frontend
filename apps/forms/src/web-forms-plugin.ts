4f8764ec6cba52805828172da281daccea5c1193
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2026-06-09 10:38:45 +1200

    remove unused file

--- apps/forms/src/web-forms-plugin.ts
+++
@@ -1,8 +0,0 @@
-import { type App } from 'vue';
-import { webFormsPlugin as webForms } from '@getodk/web-forms';
-
-export const webFormsPlugin = {
-  install(app: App) {
-    app.use(webForms);
-  },
-};
