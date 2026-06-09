c387255d9a514f87aaa77ae7763cf949077a998f
latin-panda <66472237+latin-panda@users.noreply.github.com>
2026-06-09 22:52:39 +0700

    first pass - sentry in front-end

---
+++ apps/central/src/util/sentry.js
@@ -0,0 +1,21 @@
+import { attachErrorHandler, browserTracingIntegration, init, setTag } from '@sentry/vue';
+
+const initSentry = (app, source) => {
+  const dsn = import.meta.env.VITE_SENTRY_DSN;
+  if (!dsn) {
+    return;
+  }
+
+  init({
+    dsn,
+    integrations: [browserTracingIntegration()],
+    // Captures performance timing for 1 in 5 page loads.
+    // Could be extracted to VITE_SENTRY_TRACES_SAMPLE_RATE in the future.
+    tracesSampleRate: 0.2,
+  });
+
+  setTag('source', source);
+  attachErrorHandler(app, { logErrors: true });
+};
+
+export default initSentry;
