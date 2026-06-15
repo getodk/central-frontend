e620e5a4c1e79e6ecd366c5d9ceb935af9facb65
Gareth Bowen <gbowen@getodk.org>
2026-06-15 16:36:21 +1200

    fix(web-forms#535): separate enketo and web-forms into their own vue app

---
+++ apps/forms/src/main.ts
@@ -0,0 +1,17 @@
+import type { Component } from 'vue';
+import { createApp } from 'vue';
+import PrimeVue from 'primevue/config';
+import { odkThemePreset } from '../../../packages/web-forms/src/odk-theme-preset';
+
+import { webFormsPlugin } from '@getodk/web-forms';
+import Forms from './Forms.vue';
+
+import router from './router';
+import { i18n } from './i18n'
+
+const app = createApp(Forms as Component);
+app.use(PrimeVue, { theme: { preset: odkThemePreset, options: { darkModeSelector: false } } });
+app.use(webFormsPlugin);
+app.use(i18n);
+app.use(router);
+app.mount('#forms-app');
