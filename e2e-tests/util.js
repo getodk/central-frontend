3cce0810834c073c9e784a86b38c15e3ca25b009
alxndrsn <alxndrsn>
2026-06-16 09:17:17 +0000

    e2e-tests: ignore feature-flags console messages
    Ref getodk/central#1979

--- e2e-tests/util.js
+++ e2e-tests/util.js
@@ -26,6 +26,9 @@ const test = testBase.extend({
 
         const message = msg.text();
 
+        // See: /apps/central/src/composables/feature-flags.js
+        if(message.includes('ODK Central Alpha Features:')) return;
+
         if(browserName === 'firefox') {
           // See: https://github.com/getodk/central/issues/1986
           if(message.match(/"downloadable font: glyf: Glyph bbox was incorrect;.*font-family: "FontAwesome"/)) return;
