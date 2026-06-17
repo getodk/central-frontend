353913d272776610f36151a439e57fb68e47f22b
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2026-06-17 21:01:14 +1200

    fix: output main js files to assets dir so they get cached

--- vite.config.js
+++ vite.config.js
@@ -98,9 +98,9 @@ export default defineConfig(({ mode }) => ({
         entryFileNames: (chunkInfo) => {
           const moduleId = chunkInfo.facadeModuleId;
           if (moduleId && moduleId.includes('/apps/forms/')) {
-            return 'forms/[name]-[hash].js';
+            return 'assets/forms/[name]-[hash].js';
           }
-          return 'central/[name]-[hash].js';
+          return 'assets/central/[name]-[hash].js';
         }
       },
     },
