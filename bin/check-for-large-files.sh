7df9740c6a36e9892207b9d688b24e42b1cced2a
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2026-06-25 15:09:47 +1200

    file moved

--- bin/check-for-large-files.sh
+++ bin/check-for-large-files.sh
@@ -3,7 +3,7 @@ set -o pipefail
 shopt -s inherit_errexit || true
 
 git ls-files -z -- . \
-    ':!:packages/common/src/fixtures/test-javarosa/resources/smoketests/whova_form.xml' \
+    ':!:packages/xforms-engine/test/scenario/fixtures/test-javarosa/resources/whova_form.xml' \
     ':!:packages/common/src/fixtures/test-javarosa/resources/wards.xml' \
 | xargs -0 ls -l -- \
 | awk '
