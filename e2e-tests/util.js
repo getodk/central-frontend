e730cdb3ec95e6a2755c50713d4ae3c074e8934e
alxndrsn <alxndrsn>
2026-07-14 05:59:24 +0000

    Add another exception

--- e2e-tests/util.js
+++ e2e-tests/util.js
@@ -86,6 +86,9 @@ const test = testBase.extend({
 const expectedErrors = [
   // https://github.com/getodk/central/issues/1915
   new RegExp(`Loading the image 'http://.*' violates the following Content Security Policy directive: "img-src .*https:.*".`),
+
+  // https://github.com/getodk/central/issues/2056
+  `Refused to execute script from 'http://central-test.localhost/apps/forms/src/init.js' because its MIME type ('text/html') is not executable, and strict MIME type checking is enabled.`,
 ];
 
 function gatherUnexpectedLogs(fatals, messageType, fullMessage) {
