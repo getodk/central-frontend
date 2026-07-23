a33d9e7e044137b8e8d2c83ed79045d1f59c3559
Gareth Bowen <gareth@bowenwebdesign.co.nz>
2026-07-24 08:56:09 +1200

    fix(#2084): remove unsupported namespace declaration

--- apps/central/src/styles.js
+++ apps/central/src/styles.js
@@ -11,7 +11,6 @@ except according to the terms contained in the LICENSE file.
 */
 
 // Import global styles.
-import './assets/css/namespaces.css';
 import './assets/css/bootstrap.css';
 import './assets/css/icomoon.css';
 import './assets/scss/app.scss';
