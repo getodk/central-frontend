f717c71067e6169dcb7e06dab85e0a1d1a8082cb
Gareth Bowen <gbowen@getodk.org>
2026-07-31 14:01:03 +1200

    fix(web-forms#846): setvalue order wrong on second update

--- packages/xforms-engine/src/lib/reactivity/createInstanceValueState.ts
+++ packages/xforms-engine/src/lib/reactivity/createInstanceValueState.ts
@@ -267,9 +267,13 @@ const registerSetValueActions = (
 };
 
 const registerValueChangedActions = (context: ValueContext, getValue: Accessor<string>) => {
-  context.valueChangedActions?.forEach((action) => {
-    let previous = getValue();
-    createComputed(() => {
+  if (!context.valueChangedActions?.length) {
+    return;
+  }
+  let previous: string;
+  createComputed(() => {
+    const sourceValue = getValue();
+    context.valueChangedActions.forEach((action) => {
       const ref = bindRefToRepeatInstance(context, action.ref);
       const destinationNodes = context.evaluator.evaluateNodes(ref, {
         contextNode: context.contextNode,
@@ -283,8 +287,6 @@ const registerValueChangedActions = (context: ValueContext, getValue: Accessor<s
         destinationNode.isAttached() &&
         context.isAttached()
       ) {
-        const sourceValue = getValue();
-
         if (
           previous !== undefined &&
           previous !== sourceValue &&
@@ -293,17 +295,18 @@ const registerValueChangedActions = (context: ValueContext, getValue: Accessor<s
           if (action.type === 'geopoint') {
             getGeopointValue(context, (point) => destinationNode.setEncodedValue(point));
           } else {
-            const value = destinationNode.evaluator.evaluateString(
-              action.computation.expression,
-              destinationNode
-            );
+            const value = untrack(() => {
+              return context.evaluator.evaluateString(
+                action.computation.expression,
+                destinationNode
+              );
+            });
             destinationNode.setEncodedValue(value);
           }
         }
-
-        previous = sourceValue;
       }
     });
+    previous = sourceValue;
   });
 };
 
