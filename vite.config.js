226180d268cdef664bb775056df56fba748501ee
alxndrsn <alxndrsn>
2026-08-05 09:55:53 +0000

    e2e test?

--- vite.config.js
+++ vite.config.js
@@ -118,8 +118,9 @@ export default defineConfig(({ mode }) => ({
   },
   // Not sure why this is needed in addition to build.target above and why it's
   // only an issue in development. `npm run dev` doesn't work without this.
-  optimizeDeps: mode === 'development'
-    ? { esbuildOptions: { target: buildTarget } }
-    : {},
+  optimizeDeps: {
+    include: ['zxcvbn'],
+    ...(mode === 'development' ? { esbuildOptions: { target: buildTarget } } : {}),
+  },
   server: devServer
 }));
