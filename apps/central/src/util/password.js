c1775bb4e42064b2c5edb2492c946dc3496a356f
alxndrsn <alxndrsn>
2026-08-12 07:42:18 +0000

    reorder, rename

--- apps/central/src/util/password.js
+++ apps/central/src/util/password.js
@@ -1,17 +1,5 @@
-async function getSuffixesFor(request, prefix) {
-  try {
-    const url = `https://api.pwnedpasswords.com/range/${prefix}`;
-    const res = await request({ url });
-    return res.data.split('\n').map(line => line.split(':')[0]);
-  } catch (err) {
-    console.log('pwned check failed:', err); // eslint-disable-line no-console
-    // if we can't check, just let them use it
-    return [];
-  }
-}
-
 export async function checkPasswordPwnage(request, password) { // eslint-disable-line import/prefer-default-export
-  const hash = await digestMessage(password); // eslint-disable-line no-use-before-define
+  const hash = await sha1hash(password); // eslint-disable-line no-use-before-define
 
   const hashPrefix = hash.substring(0, 5);
   const hashSuffix = hash.substring(5);
@@ -22,12 +10,24 @@ export async function checkPasswordPwnage(request, password) { // eslint-disable
 }
 
 // from: https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest#converting_a_digest_to_a_hex_string
-async function digestMessage(message) {
+async function sha1hash(message) {
   const msgUint8 = new TextEncoder().encode(message);
   const hashBuffer = await crypto.subtle.digest('SHA-1', msgUint8);
   const hashArray = Array.from(new Uint8Array(hashBuffer));
-  return hashArray
+  const hash = hashArray
     .map(b => b.toString(16).padStart(2, '0'))
     .join('')
     .toUpperCase();
+}
+
+async function getSuffixesFor(request, prefix) {
+  try {
+    const url = `https://api.pwnedpasswords.com/range/${prefix}`;
+    const res = await request({ url });
+    return res.data.split('\n').map(line => line.split(':')[0]);
+  } catch (err) {
+    console.log('pwned check failed:', err); // eslint-disable-line no-console
+    // if we can't check, just let them use it
+    return [];
+  }
 }
