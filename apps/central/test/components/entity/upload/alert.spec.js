e5a8c5e21541c9dacd142c65f271016864a1a9b6
Matthew White <matthew-white@users.noreply.github.com>
2026-08-26 14:09:13 -0400

    Add describe() block for ranges prop

--- apps/central/test/components/entity/upload/alert.spec.js
+++ apps/central/test/components/entity/upload/alert.spec.js
@@ -29,23 +29,25 @@ describe('EntityUploadAlert', () => {
     });
   });
 
-  it('lists and formats row ranges', async () => {
-    const component = mountComponent({
-      props: { ranges: [[1, 1], [1000, 1001]] }
+  describe('ranges prop', () => {
+    it('lists and formats row ranges', async () => {
+      const component = mountComponent({
+        props: { ranges: [[1, 1], [1000, 1001]] }
+      });
+      // Wait for I18nList to finish rendering.
+      await nextTick();
+      component.get('.i18n-list').text().should.equal('1, 1,000–1,001');
     });
-    // Wait for I18nList to finish rendering.
-    await nextTick();
-    component.get('.i18n-list').text().should.equal('1, 1,000–1,001');
-  });
 
-  it('renders a link for each range', async () => {
-    const component = mountComponent({
-      props: { ranges: [[1, 1], [2, 3]] }
+    it('renders a link for each range', async () => {
+      const component = mountComponent({
+        props: { ranges: [[1, 1], [2, 3]] }
+      });
+      const a = component.findAll('a');
+      a.length.should.equal(2);
+      await a[0].trigger('click');
+      await a[1].trigger('click');
+      component.emitted().rows.should.eql([[[0, 0]], [[1, 2]]]);
     });
-    const a = component.findAll('a');
-    a.length.should.equal(2);
-    await a[0].trigger('click');
-    await a[1].trigger('click');
-    component.emitted().rows.should.eql([[[0, 0]], [[1, 2]]]);
   });
 });
