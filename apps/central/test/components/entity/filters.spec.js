0385368d0ad6d603835567886b0d3ba6b279c2df
Kathleen Tuite <ktuite@getodk.org>
2026-09-02 20:46:50 -0700

    fixing setup for another test

--- apps/central/test/components/entity/filters.spec.js
+++ apps/central/test/components/entity/filters.spec.js
@@ -366,7 +366,10 @@ describe('EntityFilters', () => {
   });
 
   it('does not update dataset.entities after filtering with view-as', () => {
-    testData.extendedDatasets.createPast(1, { entities: 2 });
+    testData.extendedDatasets.createPast(1, {
+      entities: 2,
+      accessFilter: { type: 'ownerOnly' }
+    });
     testData.extendedEntities.createPast(2);
     testData.extendedFieldKeys.createPast(1);
     return load('/projects/1/entity-lists/trees/entities', {
