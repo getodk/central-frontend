361bbbb2806b808a86acfb6f2287b839e0af062d
Sadiq Khoja <sadiqkhoja@gmail.com>
2026-09-16 14:37:15 -0400

    Created PageHeading
    * used it on the Submissions data page
    * moved bunch of buttons on the Submissions data page
    * fixed a bug on draft page related to the position of the pagination

--- apps/central/src/components/submission/table-view.vue
+++ apps/central/src/components/submission/table-view.vue
@@ -20,14 +20,14 @@ except according to the terms contained in the LICENSE file.
     :filter="!!filter"
     :total-count="pagination.page ? 0 : totalCount"/>
   <!-- @update:page is emitted on size change as well -->
-  <div v-if="pagination.count > 0" class="fixed-pagination-container">
+  <teleport v-if="pagination.count > 0" to="#submission-list-pagination-target">
     <Pagination
       v-model:page="pagination.page" v-model:size="pagination.size"
       :count="pagination.count" :size-options="pageSizeOptions"
       :spinner="odata.awaitingResponse"
       :removed="pagination.removed"
       @update:page="handlePageChange"/>
-  </div>
+  </teleport>
 </template>
 
 <script setup>
